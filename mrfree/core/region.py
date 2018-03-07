770838465222c56a0e2cc8246beb5856aec3e22a
Stuming <1361046649@qq.com>
2018-03-07 17:36:51 +0800

    *NF*complete some functions

--- mrfree/core/region.py
+++ mrfree/core/region.py
@@ -1,5 +1,7 @@
 # region class
+import numpy as np
 from mrfree.core.attributes import Geometry, Scalar, Connection
+from mrfree.io import io
 
 
 class Region(object):
@@ -19,7 +21,7 @@ class Region(object):
         scalar: scalar attributes, should be an instance of class Scalar.
         connection: connection attributes, should be an instance of class Connection.
     """
-    def __init__(self, name, layer='4', source=None, space='native'):
+    def __init__(self, name, layer='L1-6', source=None, space='native'):
         """
         Init Region for further usage.
 
@@ -120,3 +122,184 @@ class Region(object):
         assert isinstance(connection, Connection), "Input 'connection' should be an instance of Connection."
         self._connection = connection
 
+    def union(self, region):
+        """
+        Merge another region into self.
+
+        Parameters
+        ----------
+            region: an instance of Region class, its layer and space should be the same as this region class.
+        """
+        assert self.layer == region.layer, "Layer of regions do not match."
+        assert self.space == region.space, "Space of regions do not match."
+
+        self.__union_anat_coords(region.anat_coords)
+        if hasattr(self, 'geometry'):
+            self.__union_geometry(region.geometry)
+        self.__union_scalar(region.scalar)
+        self.__union_connection(region.connection)
+
+    def __union_anat_coords(self, anat_coords):
+        anat_coords = np.append(self.anat_coords, anat_coords, axis=0)
+        self.anat_coords = np.unique(anat_coords, axis=0)
+
+    def __union_geometry(self, geometry):
+        self.geometry.union(geometry)
+
+    def __union_scalar(self, scalar):
+        # FIXME union scalar may need to remove duplicate elements.
+        self.scalar.aggregate(scalar)
+
+    def __union_connection(self, connection):
+        # FIXME union connection may need to remove duplicate elements.
+        self.connection.append(connection)
+
+    def intersect(self, region):
+        """
+        Intersect another region into self.
+
+        Parameters
+        ----------
+            region: an instance of Region class, its layer and space should be the same as this region class.
+        """
+        assert self.layer == region.layer, "Layer of regions do not match."
+        assert self.space == region.space, "Space of regions do not match."
+
+        self.__intersect_anat_coords(region.anat_coords)
+        if hasattr(self, 'geometry'):
+            self.__intersect_geometry(region.geometry)
+        self.__intersect_scalar(region.scalar)
+        self.__intersect_connection(region.connection)
+
+    def __intersect_anat_coords(self, anat_coords):
+        result = []
+        for i in self.anat_coords:
+            for j in anat_coords:
+                if np.all(i == j):
+                    result.append(i)
+        self.anat_coords = np.array(result)
+
+    def __intersect_geometry(self, geometry):
+        self.geometry.intersect(geometry)
+
+    def __intersect_scalar(self, scalar):
+        pass
+
+    def __intersect_connection(self, connection):
+        pass
+
+    def exclude(self, region):
+        """
+        Exclude another region out of self.
+
+        Parameters
+        ----------
+            region: an instance of Region class, its layer and space should be the same as this region class.
+        """
+        # TODO to be completed.
+        assert self.layer == region.layer, "Layer of regions do not match."
+        assert self.space == region.space, "Space of regions do not match."
+        self.exclude_anat_coords(region.anat_coords)
+        if hasattr(self, 'geometry'):
+            self.exclude_geometry(region.geometry)
+
+        self.exclude_scalar(region.scalar)
+        self.exclude_connection(region.connection)
+
+    def exclude_anat_coords(self, anat_coords):
+        result = []
+        for i in self.anat_coords:
+            match = 0
+            for j in anat_coords:
+                if np.all(i == j):
+                    match = 1
+                    break
+            if not match:
+                result.append(i)
+        self.anat_coords = np.array(result)
+
+    def exclude_geometry(self, geometry):
+        self.geometry.exclude(geometry)
+
+    def exclude_scalar(self, scalar):
+        # FIXME the part of getting data should be modified.
+        data1, name1 = self.scalar.get('all')
+        __, name2 = scalar.get('all')
+
+        result_name = list(np.array(name1)[np.in1d(name1, name2)])
+        result_data = data1[np.in1d(name1, name2)]
+
+        self.scalar.name = result_name
+        self.scalar.data = result_data
+
+    def exclude_connection(self, connection):
+        pass
+
+    @property
+    def centroid(self):
+        """
+        Calculate centroid of region in its property.
+
+        Return
+        ------
+            cen: region class that contain properties of center point.
+        """
+        cen = Region(name=self.name, layer=self.layer, source=self.source, space=self.space)
+        cen.xform = self.xform
+        cen.anat_coords = self.centroid_anat_coords
+        if hasattr(self, 'geometry'):
+            cen.geometry = self.geometry.centroid
+
+        # TODO scalar.centroid, connection.centroid
+        cen.scalar = self.scalar.centroid
+        cen.connection = self.connection.centroid
+        return cen
+
+    @property
+    def centroid_anat_coords(self):
+        """
+        Calculate centroid of region's anat_coords.
+
+        Return
+        ------
+            cen_anat_coords: centroid of anat_coords in region.
+        """
+        # TODO specify mean method.
+        cen_anat_coords = np.mean(self.anat_coords, axis=1)
+        return cen_anat_coords
+
+
+class SurfaceRegion(Region):
+    """
+
+    """
+    def load_geometry(self, surf_file):
+        # TODO specify index and label
+        coords, faces, label = io.load_surf_geom(surf_file)
+        self.geometry.coords = coords
+        self.geometry.faces = faces
+        self.geometry.index = label
+
+    def load_scalar(self, name, surf_file, label=None):
+        data = io.load_surf_scalar(surf_file, label)
+        self.scalar.set(name, data)
+
+    def save(self, save_path):
+        pass
+
+
+class VolumeRegion(Region):
+    """
+
+    """
+    def load_geometry(self, vol_file):
+        coords, xform = io.load_vol_geom(vol_file)
+        self.xform = xform
+        self.anat_coords = coords
+
+    def load_scalar(self, name, vol_file, label=None):
+        data = io.load_vol_scalar(vol_file, label)
+        self.scalar.set(name, data)
+
+    def save(self, save_path):
+        pass
