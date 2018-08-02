624c2c8b293e3793cf177f4d9781d0066800d9ed
zhenzonglei <zhenzonglei@gmail.com>
2018-08-02 18:00:03 +0800

    RF: algorithms module

--- mrfree/algorithms/region/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import tools
-from . import surf_tools
-from . import vol_tools
-from . import clusteringtools
-from . import fctools
-__all__ = ['tools', 'surf_tools', 'vol_tools', 'clusteringtools', 'fctools']
