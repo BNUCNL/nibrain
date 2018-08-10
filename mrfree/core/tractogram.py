96ae117396ee6eece2849189a30b2d374c5eab33
zhenzonglei <zhenzonglei@gmail.com>
2018-08-10 18:09:10 +0800

    RF: add save_to_cifti,nifti,gifti

--- mrfree/core/tractogram.py
+++ mrfree/core/tractogram.py
@@ -2,3 +2,91 @@
 class Tractogram(object):
     def __init__(self):
         pass
+
+
+
+
+    def update_from_tck(self, filename):
+        """ Construct Lines object by reading a TCK file
+
+        Parameters
+        ----------
+        filename: str
+            Pathstr to a TCK file
+
+        Returns
+        -------
+        self: a Lines object
+        """
+        pass
+
+    def save_to_tck(self, filename):
+        """ Save Lines object to a TCK file
+
+        Parameters
+        ----------
+        filename: str
+            Pathstr to a TCK file
+
+        Returns
+        -------
+        self: a Lines object
+        """
+        pass
+
+    def update_from_trk(self, filename):
+        """ Construct Lines object by reading a TRK file
+
+        Parameters
+        ----------
+        filename: str
+            Pathstr to a TRK file
+
+        Returns
+        -------
+        self: a Lines object
+        """
+        pass
+
+    def save_to_trk(self, filename):
+        """ Save Lines object to a TRK file
+
+        Parameters
+        ----------
+        filename: str
+            Pathstr to a TRK file
+
+        Returns
+        -------
+        self: a Lines object
+        """
+        pass
+
+    def update_from_vtk(self, filename):
+        """ Construct Lines object by reading a VTK file
+
+        Parameters
+        ----------
+        filename: str
+            Pathstr to a VTK file
+
+        Returns
+        -------
+        self: a Lines object
+        """
+        pass
+
+    def save_to_vtk(self, filename):
+        """ Save Lines object to a VTK file
+
+        Parameters
+        ----------
+        filename: str
+            Pathstr to a VTK file
+
+        Returns
+        -------
+        self: a Lines object
+        """
+        pass
+
