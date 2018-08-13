db5c30ea8047c08e8ca58d69dfe1009b76fe5ceb
zhenzonglei <zhenzonglei@gmail.com>
2018-08-13 16:51:19 +0800

    ENH: add tests for core

---
+++ mrfree/core/tests/test_geometry.py
@@ -0,0 +1,31 @@
+
+from mrfree.core.geometry import (Points,Lines)
+import numpy as np
+
+
+def test_points():
+    data = np.random.rand(10,3)
+    id = 1
+    src = "Faked points"
+    P = Points(data, id, src)
+    P.data = np.random.rand(5,3)
+    P.id = 2
+    P.src = "New faked points"
+
+
+def test_lines():
+    data = [np.array([[0, 0., 0.9],
+                  [1.9, 0., 0.]]),
+        np.array([[0.1, 0., 0],
+                  [0, 1., 1.],
+                  [0, 2., 2.]]),
+        np.array([[2, 2, 2],
+                  [3, 3, 3]])]
+    id = np.arange(len(data))
+    src = "Faked lines"
+    L = Lines(data, id, src)
+    L.data =  L.data.remove(1)
+    L.id = np.delete(L.id,1)
+    L.src = "New faked lines"
+
+
