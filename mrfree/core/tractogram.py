849465318f28d04d41c2489f9499d4b63b90aae9
Yukun Qu <532021098@qq.com>
2018-09-06 17:16:36 +0800

    "修改tractogram： 		1.修改类名字，与tck文件内Tractogram类冲突了，那个类存取都要用到 		2.修改self.lines 为self.tractography，因为line 的英文含义时直线，不适合指代纤维 		2.add load_tractogram函数读取tck文件 		3add save_tractogram 函数，可以根据想要存储的不同方式进行存储 		4.add load_data 读取 	修改geometry： 		1.add get_toi_data 根据id进行选择纤维的函数"

--- mrfree/core/tractogram.py
+++ mrfree/core/tractogram.py
@@ -1,6 +1,8 @@
+from nibabel.streamlines import tck
+from nibabel import trackvis
 
-class Tractogram(object):
-    """ Tractogram class represent fibers from fiber tracking
+class Tractography(object):
+    """ Class Tractography represents the result of fiber tracking process.
 
         Attributes
         ----------
@@ -10,62 +12,91 @@ class Tractogram(object):
         dims: image dimensions, a 3x1 or 4x1 array
         """
 
-    def __init__(self, lines=None, data=None, space=None):
+    def __init__(self, tractography=None, data=None, space=None):
         """
-
         Parameters
         ----------
         lines: a Lines object
         data: the scalar image data
-        space: str, native,mni152
+        space: str, native, mni152
         """
-        self.lines = lines
+        self.tractography = tractography
         self.data = data
         self.space = space
 
     def load_lines(self, filename):
-        """ Load tractogram from a tractogram file, include tck, trk, vtk
+        """ Load tractography from a tractography file, include tck, trk, vtk(tck file will be accepted  temporarily  )
 
         Parameters
         ----------
         filename: str
-            Pathstr to a tractogram file
+            Pathstr to a tractography file
 
         Returns
         -------
         self: a Lines object
         """
-        pass
+        self.tractography = tck.TckFile.load(filename)
 
-    def save_lines(self, filename):
-        """ Save tractogram to a tractogram file, include tck, trk, vtk
-
+    def save_tck(self,save_form,save_param,out_path=None):
+        """
+        save streamlines data
         Parameters
         ----------
-        filename: str
-            Pathstr to a tractogram file
-
-        Returns
-        -------
-        self: a Lines object
+        save_form: str, select one file format to save,include tck, trk, vtk
+        streamlines: iterable of ndarrays or :class:`ArraySequence`, optional
+                Sequence of $T$ streamlines. Each streamline is an ndarray of
+                shape ($N_t$, 3) where $N_t$ is the number of points of
+                streamline $t$.
+        header:streamlines data header
+        data_per_streamline: dict of iterable of ndarrays, optional
+                Dictionary where the items are (str, iterable).
+                Each key represents an information $i$ to be kept alongside every
+                streamline, and its associated value is an iterable of ndarrays of
+                shape ($P_i$,) where $P_i$ is the number of scalar values to store
+                for that particular information $i$.
+        data_per_point: dict of iterable of ndarrays, optional
+                Dictionary where the items are (str, iterable).
+                Each key represents an information $i$ to be kept alongside every
+                point of every streamline, and its associated value is an iterable
+                of ndarrays of shape ($N_t$, $M_i$) where $N_t$ is the number of
+                points for a particular streamline $t$ and $M_i$ is the number
+                scalar values to store for that particular information $i$.
+        affine_to_rasmm: ndarray of shape (4, 4) or None, optional
+                Transformation matrix that brings the streamlines contained in
+                this tractogram to *RAS+* and *mm* space where coordinate (0,0,0)
+                refers to the center of the voxel. By default, the streamlines
+                are in an unknown space, i.e. affine_to_rasmm is None.
+        out_path: filename for saving
+        Return
+        ------
+        streamlines data
         """
-        pass
+        if save_form == 'tck':
+            streamline = save_param[0]
+            data_per_streamline = save_param[1]
+            data_per_point = save_param[2]
+            affine_to_rasmm = save_param[3]
+            tractogram = streamlines.tractogram.Tractogram(streamlines=streamline,data_per_streamline=data_per_streamline,
+                                                        data_per_point=data_per_point,affine_to_rasmm=affine_to_rasmm)
+            datdat = nibtck.TckFile(tractogram=tractogram, header=header)
+            datdat.save(out_path)
 
-    def load_data(self, filename):
-        """ Load tractogram scalar data from a tractogram scalar file
+    def load_data(self, tractography=None):
+        """ Load fiber streamlines data from a tractography file
         Parameters
         ----------
-        filename: str
-            Pathstr to a tractogram file
+        tractography: str of filepath or line object
 
-        Returns
-        -------
-        self: a Tractogram object
         """
-        pass
+        if tractography == None:
+            self.data = self.lines.streamlines
+        else:
+            self.tractography = tck.TckFile.load(tractography)
+            self.data = self.tractography.streamlines
 
     def save_data(self, filename):
-        """ Save tractogram scalar data from a tractogram scalar file
+        """ Save tractogram scalar data to a tractogram scalar file
         Parameters
         ----------
         filename: str
@@ -77,9 +108,8 @@ class Tractogram(object):
         """
         pass
     
-    
     def get_toi_data(self, toi=None):
-        """ Get the scalar data of the fiber within a toi
+        """Get the coordinates of the node within a toi
 
         Parameters
         ----------
