5052bcdc3eb1a9a498d193e5dd340ce8996b12ee
sunshineDrizzle <954830460@qq.com>
2021-02-22 02:55:22 -0500

    compare face selectivity difference for G1 and G2

--- cxy_hcp_ffa/scripts/grouping_activation.py
+++ cxy_hcp_ffa/scripts/grouping_activation.py
@@ -27,7 +27,8 @@ def split_half():
             indices = np.where(gid_idx_vec)[0]
             n_gid = len(indices)
             gids[gid_idx_vec] = gh1_id
-            half2_indices = np.random.choice(indices, int(n_gid/2), replace=False)
+            half2_indices = np.random.choice(indices, int(n_gid/2),
+                                             replace=False)
             gids[half2_indices] = gh2_id
 
         print(f'The size of group1-half1-{hemi}:', np.sum(gids == 11))
@@ -102,6 +103,87 @@ def get_mpm(gh_id=11, hemi='lh'):
     save2nifti(out_file, mpm)
 
 
+def calc_meas(gid=1, hemi='lh'):
+    """
+    用一半被试的MPM去提取另一半被试的激活值
+    如果某个被试没有某个ROI，就不提取该被试该ROI的信号
+
+    Args:
+        gid (int, optional): group ID. Defaults to '1'.
+        hemi (str, optional): hemisphere. Defaults to 'lh'.
+    """
+    import numpy as np
+    import pickle as pkl
+    import nibabel as nib
+    from cxy_hcp_ffa.lib.predefine import hemi2stru, roi2label
+    from commontool.io.io import CiftiReader
+
+    gh_ids = (gid*10+1, gid*10+2)
+    gh_id_file = pjoin(split_dir, f'half_id_{hemi}.npy')
+    mpm_file = pjoin(split_dir, 'MPM_GH{gh_id}_{hemi}.nii.gz')
+    roi_file = pjoin(proj_dir, 'analysis/s2/1080_fROI/refined_with_Kevin/'
+                               f'rois_v3_{hemi}.nii.gz')
+    subj_file = pjoin(proj_dir, 'analysis/s2/subject_id')
+    src_file = pjoin(proj_dir, 'analysis/s2/activation.dscalar.nii')
+    trg_file = pjoin(work_dir, f'G{gid}_activ_{hemi}.pkl')
+
+    gh_id_vec = np.load(gh_id_file)
+    gid_idx_vec = np.logical_or(gh_id_vec == gh_ids[0], gh_id_vec == gh_ids[1])
+    hid_vec = gh_id_vec[gid_idx_vec]
+    subj_ids = open(subj_file).read().splitlines()
+    subj_ids = [subj_ids[i] for i in np.where(gid_idx_vec)[0]]
+    n_subj = len(subj_ids)
+    roi_maps = nib.load(roi_file).get_data().squeeze().T[gid_idx_vec]
+    meas_reader = CiftiReader(src_file)
+    meas = meas_reader.get_data(hemi2stru[hemi], True)[gid_idx_vec]
+
+    out_dict = {'shape': 'n_roi x n_subj',
+                'roi': list(roi2label.keys()),
+                'subject': subj_ids,
+                'meas': np.ones((len(roi2label), n_subj)) * np.nan}
+    for gh_id in gh_ids:
+        hid_idx_vec = hid_vec == gh_id
+        mpm = nib.load(mpm_file.format(gh_id=gh_id, hemi=hemi)
+                       ).get_data().squeeze()
+        for roi_idx, roi in enumerate(out_dict['roi']):
+            roi_idx_vec = np.any(roi_maps == roi2label[roi], 1)
+            valid_idx_vec = np.logical_and(~hid_idx_vec, roi_idx_vec)
+            mpm_idx_vec = mpm == roi2label[roi]
+            meas_masked = meas[valid_idx_vec][:, mpm_idx_vec]
+            out_dict['meas'][roi_idx][valid_idx_vec] = np.mean(meas_masked, 1)
+    pkl.dump(out_dict, open(trg_file, 'wb'))
+
+
+def pre_ANOVA(gid=1):
+    """
+    准备好二因素被试间设计方差分析需要的数据。
+    半球x脑区
+    """
+    import numpy as np
+    import pandas as pd
+    import pickle as pkl
+
+    hemis = ('lh', 'rh')
+    rois = ('pFus-face', 'mFus-face')
+    src_file = pjoin(work_dir, 'G{}_activ_{}.pkl')
+    trg_file = pjoin(work_dir, f'G{gid}_activ_preANOVA.csv')
+
+    out_dict = {'hemi': [], 'roi': [], 'meas': []}
+    for hemi in hemis:
+        data = pkl.load(open(src_file.format(gid, hemi), 'rb'))
+        for roi in rois:
+            roi_idx = data['roi'].index(roi)
+            meas_vec = data['meas'][roi_idx]
+            meas_vec = meas_vec[~np.isnan(meas_vec)]
+            n_valid = len(meas_vec)
+            out_dict['hemi'].extend([hemi] * n_valid)
+            out_dict['roi'].extend([roi.split('-')[0]] * n_valid)
+            out_dict['meas'].extend(meas_vec)
+            print(f'{hemi}_{roi}:', n_valid)
+    out_df = pd.DataFrame(out_dict)
+    out_df.to_csv(trg_file, index=False)
+
+
 if __name__ == '__main__':
     # split_half()
     # roi_stats(gh_id=11, hemi='lh')
@@ -116,7 +198,13 @@ if __name__ == '__main__':
     # get_mpm(gh_id=11, hemi='rh')
     # get_mpm(gh_id=21, hemi='lh')
     # get_mpm(gh_id=21, hemi='rh')
-    get_mpm(gh_id=12, hemi='lh')
-    get_mpm(gh_id=12, hemi='rh')
-    get_mpm(gh_id=22, hemi='lh')
-    get_mpm(gh_id=22, hemi='rh')
+    # get_mpm(gh_id=12, hemi='lh')
+    # get_mpm(gh_id=12, hemi='rh')
+    # get_mpm(gh_id=22, hemi='lh')
+    # get_mpm(gh_id=22, hemi='rh')
+    # calc_meas(gid=1, hemi='lh')
+    # calc_meas(gid=1, hemi='rh')
+    # calc_meas(gid=2, hemi='lh')
+    # calc_meas(gid=2, hemi='rh')
+    pre_ANOVA(gid=1)
+    pre_ANOVA(gid=2)
