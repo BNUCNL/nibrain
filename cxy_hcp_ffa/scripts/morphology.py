7b97ea1728103506401a14270a0885cd37eb7514
sunshineDrizzle <954830460@qq.com>
2021-02-24 03:16:58 -0500

    preparation for 2-way rm ANOVA for individual TMV

--- cxy_hcp_ffa/scripts/morphology.py
+++ cxy_hcp_ffa/scripts/morphology.py
@@ -84,6 +84,45 @@ def pre_ANOVA(morph='thickness'):
     out_df.to_csv(trg_file, index=False)
 
 
+def pre_ANOVA_rm_individual(morph='thickness'):
+    """
+    Preparation for two-way repeated-measures ANOVA
+    半球x脑区
+    Only the subjects who have all four ROIs will be used.
+    """
+    import numpy as np
+    import pandas as pd
+    import pickle as pkl
+
+    # inputs
+    hemis = ('lh', 'rh')
+    rois = ('pFus-face', 'mFus-face')
+    src_file = pjoin(work_dir, 'individual_{}_{}.pkl')
+    roi_idx_file = pjoin(proj_dir, 'analysis/s2/1080_fROI/refined_with_Kevin/'
+                                   'rois_v3_idx_vec.csv')
+
+    # outputs
+    trg_file = pjoin(work_dir, f'individual_{morph}_preANOVA-rm.csv')
+
+    # load data
+    roi_idx_df = pd.read_csv(roi_idx_file)
+    roi_cols = [f'{i}_{j}' for i in hemis for j in rois]
+    roi_idx_vec = np.all(roi_idx_df.loc[:, roi_cols], 1)
+
+    out_dict = {}
+    nan_idx_vec = np.zeros_like(roi_idx_vec, np.bool)
+    for hemi in hemis:
+        data = pkl.load(open(src_file.format(morph, hemi), 'rb'))
+        for roi in rois:
+            roi_idx = data['roi'].index(roi)
+            meas_vec = data['meas'][roi_idx]
+            out_dict[f"{hemi}_{roi.split('-')[0]}"] = meas_vec[roi_idx_vec]
+            nan_idx_vec = np.logical_or(nan_idx_vec, np.isnan(meas_vec))
+    assert np.all(roi_idx_vec == ~nan_idx_vec)
+    out_df = pd.DataFrame(out_dict)
+    out_df.to_csv(trg_file, index=False)
+
+
 if __name__ == '__main__':
     # calc_morphology_individual(hemi='lh', morph='thickness')
     # calc_morphology_individual(hemi='lh', morph='myelin')
@@ -91,5 +130,8 @@ if __name__ == '__main__':
     # calc_morphology_individual(hemi='rh', morph='myelin')
     # pre_ANOVA(morph='thickness')
     # pre_ANOVA(morph='myelin')
-    calc_morphology_individual(hemi='lh', morph='va')
-    calc_morphology_individual(hemi='rh', morph='va')
+    # calc_morphology_individual(hemi='lh', morph='va')
+    # calc_morphology_individual(hemi='rh', morph='va')
+    pre_ANOVA_rm_individual(morph='thickness')
+    pre_ANOVA_rm_individual(morph='myelin')
+    pre_ANOVA_rm_individual(morph='va')
