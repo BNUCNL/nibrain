9212372d73ed6825ff38c39f1a4142a559717733
sunshineDrizzle <954830460@qq.com>
2021-03-15 05:19:17 -0400

    plot mean RSFC across 12 networks

--- cxy_hcp_ffa/scripts/plot_rsfc.py
+++ cxy_hcp_ffa/scripts/plot_rsfc.py
@@ -1,4 +1,5 @@
 from os.path import join as pjoin
+from matplotlib import pyplot as plt
 
 proj_dir = '/nfs/t3/workingshop/chenxiayu/study/FFA_pattern'
 work_dir = pjoin(proj_dir, 'analysis/s2/1080_fROI/refined_with_Kevin')
@@ -11,7 +12,6 @@ def plot_radar():
     """
     import numpy as np
     import pickle as pkl
-    from matplotlib import pyplot as plt
 
     # inputs
     gids = [-1, 1, 2]
@@ -72,5 +72,67 @@ def plot_radar():
         print(lbl, name)
 
 
+def plot_bar():
+    """
+    把整组(-1)的和分组(1, 2)的都画到一起
+    """
+    import numpy as np
+    import pickle as pkl
+    from scipy.stats import sem
+    from nibrain.util.plotfig import auto_bar_width
+
+    # inputs
+    gids = (-1, 1, 2)
+    hemis = ('lh', 'rh')
+    seeds = ('pFus-face', 'mFus-face')
+    seed2color = {'pFus-face': 'limegreen', 'mFus-face': 'cornflowerblue'}
+    file1 = pjoin(work_dir, 'rfMRI/rsfc_individual2Cole_{hemi}.pkl')
+    file2 = pjoin(work_dir, 'grouping/rfMRI/'
+                            'rsfc_individual2Cole_G{gid}_{hemi}.pkl')
+
+    # outputs
+    out_file = pjoin(out_dir, 'bar_G{}_mean-across-network.jpg')
+
+    n_hemi = len(hemis)
+    n_seed = len(seeds)
+    x = np.arange(n_hemi)
+    width = auto_bar_width(x, n_seed)
+    for gid_idx, gid in enumerate(gids):
+        plt.figure(gid, figsize=(1.4, 3))
+        ax = plt.gca()
+        if gid == -1:
+            hemi2meas = {
+                'lh': pkl.load(open(file1.format(hemi='lh'), 'rb')),
+                'rh': pkl.load(open(file1.format(hemi='rh'), 'rb'))}
+        else:
+            hemi2meas = {
+                'lh': pkl.load(open(file2.format(gid=gid, hemi='lh'), 'rb')),
+                'rh': pkl.load(open(file2.format(gid=gid, hemi='rh'), 'rb'))}
+
+        offset = -(n_seed - 1) / 2
+        for seed in seeds:
+            y = np.zeros(n_hemi)
+            y_err = np.zeros(n_hemi)
+            for hemi_idx, hemi in enumerate(hemis):
+                meas = np.mean(hemi2meas[hemi][seed], 1)
+                meas = meas[~np.isnan(meas)]
+                y[hemi_idx] = np.mean(meas)
+                y_err[hemi_idx] = sem(meas)
+            ax.bar(x+width*offset, y, width, yerr=y_err,
+                   label=seed.split('-')[0], color=seed2color[seed])
+            offset += 1
+        ax.set_xticks(x)
+        ax.set_xticklabels(hemis)
+        ax.set_ylabel('RSFC')
+        ax.set_ylim(0.1)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        # ax.legend()
+        plt.tight_layout()
+        plt.savefig(out_file.format(gid))
+    # plt.show()
+
+
 if __name__ == '__main__':
-    plot_radar()
+    # plot_radar()
+    plot_bar()
