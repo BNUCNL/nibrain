cda900fbfdcf9ee848338927c5c46448648e2f16
sunshineDrizzle <954830460@qq.com>
2021-04-23 00:20:16 -0400

    NF: get_subject_info_from_completeness

--- cxy_hcp_ffa/scripts/development.py
+++ cxy_hcp_ffa/scripts/development.py
@@ -43,6 +43,44 @@ def get_subject_info_from_fmriresults01(proj_name='HCPD'):
     out_df.to_csv(trg_file, index=False)
 
 
+def get_subject_info_from_completeness(proj_name='HCPD'):
+    """Get subject information from HCD_LS_2.0_subject_completeness.csv or
+    HCA_LS_2.0_subject_completeness.csv.
+
+    Args:
+        proj_name (str, optional): Project name. Defaults to 'HCPD'.
+    """
+    import pandas as pd
+    from decimal import Decimal, ROUND_HALF_UP
+
+    # prepare
+    name2file = {
+        'HCPD': '/nfs/e1/HCPD/HCD_LS_2.0_subject_completeness.csv',
+        'HCPA': '/nfs/e1/HCPA/HCA_LS_2.0_subject_completeness.csv'}
+    src_file = name2file[proj_name]
+    trg_file = pjoin(work_dir, f'{proj_name}_SubjInfo_completeness.csv')
+
+    # calculate
+    df = pd.read_csv(src_file)
+    df.drop(labels=[0], axis=0, inplace=True)
+    assert df['src_subject_id'].to_list() == sorted(df['src_subject_id'])
+    ages_year = [int(Decimal(str(int(age)/12)).quantize(
+                 Decimal('0'), rounding=ROUND_HALF_UP))
+                 for age in df['interview_age']]
+    out_dict = {
+        'subID': df['src_subject_id'],
+        'age in months': df['interview_age'],
+        'age in years': ages_year,
+        'gender': df['sex']
+    }
+
+    # save
+    out_df = pd.DataFrame(out_dict)
+    out_df.to_csv(trg_file, index=False)
+
+
 if __name__ == '__main__':
-    get_subject_info_from_fmriresults01(proj_name='HCPD')
-    get_subject_info_from_fmriresults01(proj_name='HCPA')
+    # get_subject_info_from_fmriresults01(proj_name='HCPD')
+    # get_subject_info_from_fmriresults01(proj_name='HCPA')
+    get_subject_info_from_completeness(proj_name='HCPD')
+    get_subject_info_from_completeness(proj_name='HCPA')
