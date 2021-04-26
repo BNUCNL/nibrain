b65e94ea3fbf59b7eabdd4683c4dd4a7267f0592
masai1998 <202028061022@mail.bnu.edu.cn>
2021-04-26 19:54:45 +0800

    Update timeseries_ev_corr.py

--- ms_motor_map/scripts/motor_pipeline/timeseries_ev_corr.py
+++ ms_motor_map/scripts/motor_pipeline/timeseries_ev_corr.py
@@ -49,9 +49,42 @@ def compute_corr(events_series, time_series_list, figures_dir):
 
 
 if __name__ == '__main__':
-    ica_dir = '/nfs/e4/function_guided_resection/MotorMap/data/bold/derivatives/melodic/sub-01/ses-1/run-2.ica'
-    events_file = '/nfs/e4/function_guided_resection/MotorMapping/sub-M01/ses-01/func/sub-M01_ses-01_task-motor_run-4_events.tsv'
+
+    project_dir = '/nfs/e4/function_guided_resection/MotorMap'
+    nifti_dir = os.path.join(project_dir, 'data', 'bold', 'nifti')
+    melodic_dir = os.path.join(project_dir, 'data', 'bold', 'derivatives', 'melodic')
+
+    subjid_list = [sub.replace("sub-", "") for sub in os.listdir(nifti_dir) if "sub-" in sub]
+    runid_list = [sub.replace("sub-", "") for sub in os.listdir(nifti_dir) if "sub-" in sub]
+
+
+
+
+
+
+
+
+    ica_dir = '/nfs/e4/function_guided_resection/MotorMap/data/bold/derivatives/melodic/sub-01/ses-1/run-1.ica'
+    events_file = '/nfs/e4/function_guided_resection/MotorMap/data/bold/nifti/sub-01/ses-1/func/sub-01_ses-1_task-motor_run-01_events.tsv'
+    """
+        -------------------
+        we promise:
+        0: fixation
+        1: toe
+        2: ankle
+        3: leftleg
+        4: rightleg
+        5: forearm
+        6: upperarm
+        7: wrist
+        8: finger
+        9: eye
+        10: jaw
+        11: lip
+        12: tongue
+    """
     target_ev_list = [3, 4]
-    figures_dir = '/nfs/e2/workingshop/masai/melodic_test/test1'
+    # target_ev_list = [9, 10, 11, 12]
+    figures_dir = '/nfs/e4/function_guided_resection/MotorMap/data/bold/derivatives/melodic/sub-01/ses-1/run-1.ica/task_related_report/leg'
     events_series, time_series_list = prepare_data(ica_dir, events_file, target_ev_list)
     compute_corr(events_series, time_series_list, figures_dir)
