30fab6c89872ed2dc1fb3a62c1e891dd89c9268b
sunshineDrizzle <954830460@qq.com>
2021-05-12 03:31:20 -0400

    cxy_visual_dev: extract_net_parcel_info

--- cxy_visual_dev/scripts/explore_ColeNetwork.py
+++ cxy_visual_dev/scripts/explore_ColeNetwork.py
@@ -28,6 +28,9 @@ def separate_networks():
     """
     把ColeNetwork的12个网络分到单独的map里。
     每个map的MMP parcel的label保留原来的样子。
+    需要注意的是multimodal_glasser的MMP_mpmLR32k.dlabel.nii中，
+    ROI label的编号1~180是右脑，181~360是左脑。0对应的是???
+    而cortex_parcel_network_assignments.mat中0~359的index是左脑在先
     """
     import numpy as np
     import nibabel as nib
@@ -75,5 +78,36 @@ def separate_networks():
                map_names, label_tables=label_tables)
 
 
+def extract_net_parcel_info():
+    """
+    把每个网络的编号，名字以及所包含的MMP parcel的编号和名字列出来
+    """
+    from magicbox.io.io import CiftiReader
+
+    # inputs
+    net_file = pjoin(work_dir, 'networks.dlabel.nii')
+
+    # outputs
+    out_file = pjoin(work_dir, 'net_parcel_info.txt')
+
+    # prepare
+    reader = CiftiReader(net_file)
+    net_names = reader.map_names()
+    lbl_tables = reader.label_tables()
+
+    # calculate & save
+    wf = open(out_file, 'w')
+    for net_idx, net_name in enumerate(net_names):
+        wf.write(f'>>>{net_idx+1}-{net_name}\n')
+        lbl_tab = lbl_tables[net_idx]
+        for lbl_k, lbl_v in lbl_tab.items():
+            if lbl_k == 0:
+                continue
+            wf.write(f'{lbl_k}-{lbl_v.label[:-4]}\n')
+        wf.write('<<<\n')
+    wf.close()
+
+
 if __name__ == '__main__':
-    separate_networks()
+    # separate_networks()
+    extract_net_parcel_info()
