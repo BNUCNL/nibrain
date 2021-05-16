925bc5f0f5107af48690c018f5acf9c7e652c004
sunshineDrizzle <954830460@qq.com>
2021-05-16 03:05:52 -0400

    cxy_hcp_ffa: calculate RSFC between MPM and ColeNet

--- cxy_hcp_ffa/scripts/dev_rfMRI.py
+++ cxy_hcp_ffa/scripts/dev_rfMRI.py
@@ -195,13 +195,140 @@ def prepare_series(sess=1, run='AP'):
     pkl.dump(global_dict, open(out_global, 'wb'))
 
 
+def rsfc(sess=1, run='AP', hemi='lh'):
+    """
+    计算静息态功能连接
+    """
+    import numpy as np
+    import pandas as pd
+    import pickle as pkl
+    from scipy.spatial.distance import cdist
+
+    # prepare seeds
+    seed_file = pjoin(work_dir, f'rfMRI{sess}_{run}_MPM_{hemi}.pkl')
+    seed_dict = pkl.load(open(seed_file, 'rb'))
+
+    # prepare targets
+    trg_file = pjoin(work_dir, f'rfMRI{sess}_{run}_MMP.pkl')
+    trg_dict = pkl.load(open(trg_file, 'rb'))
+    n_trg = len(trg_dict['trg_label'])
+
+    assert seed_dict['subject'] == trg_dict['subject']
+
+    # prepare FC dictionary
+    subj_info_file = pjoin(dev_dir, 'HCPD_SubjInfo.csv')
+    subj_ids = pd.read_csv(subj_info_file)['subID'].to_list()
+    n_subj = len(subj_ids)
+    fc_dict = {
+        'shape': 'n_subject x n_target',
+        'subject': subj_ids,
+        'trg_label': trg_dict['trg_label']}
+    for seed in seed_dict['seed']:
+        fc_dict[seed] = np.ones((n_subj, n_trg)) * np.nan
+
+    # start
+    subj_ids_valid = seed_dict['subject']
+    n_valid = len(subj_ids_valid)
+    for valid_idx, valid_id in enumerate(subj_ids_valid):
+        print('Progress: {}/{}'.format(valid_idx+1, n_valid))
+        subj_idx = subj_ids.index(valid_id)
+        for seed_idx, seed in enumerate(seed_dict['seed']):
+            seed_series = seed_dict['rfMRI'][valid_idx, [seed_idx]]
+            if not np.isnan(seed_series[0, 0]):
+                fc = 1 - cdist(seed_series, trg_dict['rfMRI'][valid_idx],
+                               metric='correlation')[0]
+                fc_dict[seed][subj_idx] = fc
+
+    # save
+    out_file = pjoin(work_dir, f'rsfc_MPM2MMP_{hemi}_{sess}_{run}.pkl')
+    pkl.dump(fc_dict, open(out_file, 'wb'))
+
+
+def rsfc_mean_among_run(hemi='lh'):
+    import numpy as np
+    import pickle as pkl
+
+    # inputs
+    fnames = [f'rsfc_MPM2MMP_{hemi}_1_AP.pkl', f'rsfc_MPM2MMP_{hemi}_1_PA.pkl',
+              f'rsfc_MPM2MMP_{hemi}_2_AP.pkl', f'rsfc_MPM2MMP_{hemi}_2_PA.pkl']
+    fpaths = [pjoin(work_dir, fname) for fname in fnames]
+    rois = ['IOG-face', 'pFus-face', 'mFus-face']
+
+    # outputs
+    out_file = pjoin(work_dir, f'rsfc_MPM2MMP_{hemi}.pkl')
+
+    # calculate
+    rsfc_dict = dict()
+    for idx, f in enumerate(fpaths):
+        tmp_rsfc = pkl.load(open(f, 'rb'))
+        if idx == 0:
+            rsfc_dict['shape'] = tmp_rsfc['shape']
+            rsfc_dict['subject'] = tmp_rsfc['subject']
+            rsfc_dict['trg_label'] = tmp_rsfc['trg_label']
+            for roi in rois:
+                rsfc_dict[roi] = [tmp_rsfc[roi]]
+        else:
+            assert rsfc_dict['shape'] == tmp_rsfc['shape']
+            assert rsfc_dict['subject'] == tmp_rsfc['subject']
+            assert rsfc_dict['trg_label'] == tmp_rsfc['trg_label']
+            for roi in rois:
+                rsfc_dict[roi].append(tmp_rsfc[roi])
+    for roi in rois:
+        rsfc_dict[roi] = np.mean(rsfc_dict[roi], 0)
+
+    # save
+    pkl.dump(rsfc_dict, open(out_file, 'wb'))
+
+
+def rsfc_merge_MMP(hemi='lh'):
+    """
+    用ColeAnticevicNetPartition将MMP合并成12个网络
+    """
+    import numpy as np
+    import pickle as pkl
+    from scipy.io import loadmat
+
+    rsfc_file = pjoin(work_dir, f'rsfc_MPM2MMP_{hemi}.pkl')
+    rsfc_dict = pkl.load(open(rsfc_file, 'rb'))
+
+    roi2net_file = '/nfs/p1/atlases/ColeAnticevicNetPartition/'\
+        'cortex_parcel_network_assignments.mat'
+    roi2net = loadmat(roi2net_file)['netassignments'][:, 0]
+    roi2net = np.r_[roi2net[180:], roi2net[:180]]
+    net_labels = sorted(set(roi2net))
+    n_net = len(net_labels)
+
+    seeds = ('IOG-face', 'pFus-face', 'mFus-face')
+    for seed in seeds:
+        data = np.zeros((rsfc_dict[seed].shape[0], n_net))
+        for net_idx, net_lbl in enumerate(net_labels):
+            data[:, net_idx] = np.mean(rsfc_dict[seed][:, roi2net == net_lbl], 1)
+        rsfc_dict[seed] = data
+    rsfc_dict['trg_label'] = net_labels
+
+    out_file = pjoin(work_dir, f'rsfc_MPM2Cole_{hemi}.pkl')
+    pkl.dump(rsfc_dict, open(out_file, 'wb'))
+
+
 if __name__ == '__main__':
     # get_valid_id(sess=1, run='AP')
     # get_valid_id(sess=1, run='PA')
     # get_valid_id(sess=2, run='AP')
     # get_valid_id(sess=2, run='PA')
     # get_common_id()
-    prepare_series(sess=1, run='AP')
-    prepare_series(sess=1, run='PA')
-    prepare_series(sess=2, run='AP')
-    prepare_series(sess=2, run='PA')
+    # prepare_series(sess=1, run='AP')
+    # prepare_series(sess=1, run='PA')
+    # prepare_series(sess=2, run='AP')
+    # prepare_series(sess=2, run='PA')
+    rsfc(sess=1, run='AP', hemi='lh')
+    rsfc(sess=1, run='AP', hemi='rh')
+    rsfc(sess=1, run='PA', hemi='lh')
+    rsfc(sess=1, run='PA', hemi='rh')
+    rsfc(sess=2, run='AP', hemi='lh')
+    rsfc(sess=2, run='AP', hemi='rh')
+    rsfc(sess=2, run='PA', hemi='lh')
+    rsfc(sess=2, run='PA', hemi='rh')
+    rsfc_mean_among_run(hemi='lh')
+    rsfc_mean_among_run(hemi='rh')
+    rsfc_merge_MMP(hemi='lh')
+    rsfc_merge_MMP(hemi='rh')
