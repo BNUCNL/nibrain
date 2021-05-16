7ac5aa3fc8c7f98d5762cb5995db47b281708c20
sunshineDrizzle <954830460@qq.com>
2021-05-15 22:03:45 -0400

    cxy_hcp_ffa: prepare rfMRI series

--- cxy_hcp_ffa/scripts/dev_rfMRI.py
+++ cxy_hcp_ffa/scripts/dev_rfMRI.py
@@ -62,7 +62,7 @@ def get_common_id():
     fpaths = [pjoin(work_dir, fname) for fname in fnames]
 
     # outputs
-    out_file = pjoin(work_dir, 'rfMRI_REST_4run')
+    out_file = pjoin(work_dir, 'rfMRI_REST_4run_id')
 
     # calculate
     ids = set(open(fpaths[0]).read().splitlines())
@@ -75,9 +75,133 @@ def get_common_id():
         wf.write(ids)
 
 
+def prepare_series(sess=1, run='AP'):
+    import numpy as np
+    import nibabel as nib
+    import pickle as pkl
+    from cxy_hcp_ffa.lib.predefine import hemi2stru, roi2label
+    from magicbox.io.io import CiftiReader
+
+    # prepare seeds
+    seeds = ('IOG-face', 'pFus-face', 'mFus-face')
+    n_seed = len(seeds)
+    seed_mask_lh_file = pjoin(proj_dir, 'analysis/s2/1080_fROI/'
+                              'refined_with_Kevin/MPM_v3_lh_0.25.nii.gz')
+    seed_mask_rh_file = pjoin(proj_dir, 'analysis/s2/1080_fROI/'
+                              'refined_with_Kevin/MPM_v3_rh_0.25.nii.gz')
+    hemi2seed_mask = {
+        'lh': nib.load(seed_mask_lh_file).get_fdata().squeeze(),
+        'rh': nib.load(seed_mask_rh_file).get_fdata().squeeze()}
+
+    # prepare targets
+    trg_file = '/nfs/p1/atlases/multimodal_glasser/surface/'\
+        'MMP_mpmLR32k.dlabel.nii'
+    trg_reader = CiftiReader(trg_file)
+    hemi2trg_mask = {
+        'lh': trg_reader.get_data(hemi2stru['lh'], True).squeeze(),
+        'rh': trg_reader.get_data(hemi2stru['rh'], True).squeeze()}
+
+    trg_labels = np.unique(np.r_[hemi2trg_mask['lh'], hemi2trg_mask['rh']])
+    trg_labels = trg_labels[trg_labels != 0].astype(int).tolist()
+    n_trg = len(trg_labels)
+
+    # prepare series dictionary
+    subj_id_file = pjoin(work_dir, 'rfMRI_REST_4run_id')
+    subj_ids = open(subj_id_file).read().splitlines()
+    n_subj = len(subj_ids)
+    n_tp = 478  # the number of time points
+
+    seed_lh_dict = {
+        'shape': 'n_subject x n_seed x n_time_point',
+        'subject': subj_ids,
+        'seed': seeds,
+        'rfMRI': np.ones((n_subj, n_seed, n_tp)) * np.nan
+    }
+    seed_rh_dict = {
+        'shape': 'n_subject x n_seed x n_time_point',
+        'subject': subj_ids,
+        'seed': seeds,
+        'rfMRI': np.ones((n_subj, n_seed, n_tp)) * np.nan
+    }
+    hemi2seed_dict = {
+        'lh': seed_lh_dict,
+        'rh': seed_rh_dict
+    }
+    trg_dict = {
+        'shape': 'n_subject x n_target x n_time_point',
+        'subject': subj_ids,
+        'trg_label': trg_labels,
+        'rfMRI': np.ones((n_subj, n_trg, n_tp)) * np.nan
+    }
+    global_dict = {
+        'shape': 'n_subject x n_global x n_time_point',
+        'subject': subj_ids,
+        'global': ['lr', 'lh', 'rh'],
+        'rfMRI': np.ones((n_subj, 3, n_tp)) * np.nan
+    }
+
+    # prepare outputs
+    out_seed_lh = pjoin(work_dir, f'rfMRI{sess}_{run}_MPM_lh.pkl')
+    out_seed_rh = pjoin(work_dir, f'rfMRI{sess}_{run}_MPM_rh.pkl')
+    out_trg = pjoin(work_dir, f'rfMRI{sess}_{run}_MMP.pkl')
+    out_global = pjoin(work_dir, f'rfMRI{sess}_{run}_global.pkl')
+
+    # start
+    maps_files = '/nfs/e1/HCPD/fmriresults01/{0}/MNINonLinear/'\
+        'Results/rfMRI_REST{1}_{2}/'\
+        'rfMRI_REST{1}_{2}_Atlas_MSMAll_hp0_clean.dtseries.nii'
+    for subj_idx, subj_id in enumerate(subj_ids):
+        print('Progress: {}/{}'.format(subj_idx+1, n_subj))
+
+        # prepare maps
+        maps_file = maps_files.format(f'{subj_id}_V1_MR', sess, run)
+        maps_reader = CiftiReader(maps_file)
+        hemi2maps = {
+            'lh': maps_reader.get_data(hemi2stru['lh'], True),
+            'rh': maps_reader.get_data(hemi2stru['rh'], True)}
+
+        for hemi in ['lh', 'rh']:
+
+            maps = hemi2maps[hemi]
+
+            # seed dict
+            seed_mask = hemi2seed_mask[hemi]
+            for seed_idx, seed in enumerate(seeds):
+                lbl = roi2label[seed]
+                hemi2seed_dict[hemi]['rfMRI'][subj_idx, seed_idx] =\
+                    np.mean(maps[:, seed_mask == lbl], 1)
+
+            # target dict
+            trg_mask = hemi2trg_mask[hemi]
+            tmp_labels = np.unique(trg_mask)
+            tmp_labels = tmp_labels[tmp_labels != 0].astype(int)
+            for lbl in tmp_labels:
+                trg_idx = trg_labels.index(lbl)
+                trg_dict['rfMRI'][subj_idx, trg_idx] =\
+                    np.mean(maps[:, trg_mask == lbl], 1)
+
+            # global dict
+            global_idx = global_dict['global'].index(hemi)
+            global_dict['rfMRI'][subj_idx, global_idx] = np.mean(maps, 1)
+
+        maps = np.c_[hemi2maps['lh'], hemi2maps['rh']]
+        global_idx = global_dict['global'].index('lr')
+        global_dict['rfMRI'][subj_idx, global_idx] = np.mean(maps, 1)
+        del maps
+
+    pkl.dump(hemi2seed_dict['lh'], open(out_seed_lh, 'wb'))
+    pkl.dump(hemi2seed_dict['rh'], open(out_seed_rh, 'wb'))
+    pkl.dump(trg_dict, open(out_trg, 'wb'))
+    pkl.dump(global_dict, open(out_global, 'wb'))
+
+
 if __name__ == '__main__':
     # get_valid_id(sess=1, run='AP')
     # get_valid_id(sess=1, run='PA')
     # get_valid_id(sess=2, run='AP')
     # get_valid_id(sess=2, run='PA')
-    get_common_id()
+    # get_common_id()
+    prepare_series(sess=1, run='AP')
+    prepare_series(sess=1, run='PA')
+    prepare_series(sess=2, run='AP')
+    prepare_series(sess=2, run='PA')
