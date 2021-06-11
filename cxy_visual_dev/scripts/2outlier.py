84ce82d60d5d2bb78101081eb9c3031d1a0cd671
sunshineDrizzle <954830460@qq.com>
2021-06-11 01:18:55 -0400

    cxy_visual_dev: find_outlier_per_subject_IQR and plot_outlier_distribution

--- cxy_visual_dev/scripts/2outlier.py
+++ cxy_visual_dev/scripts/2outlier.py
@@ -1,4 +1,5 @@
 import os
+import time
 import numpy as np
 import pandas as pd
 import nibabel as nib
@@ -41,6 +42,7 @@ def plot_box_violin(meas_name, roi_name):
         meas_name (str): thickness | myelin
         roi_name (str): L_cole_visual | R_cole_visual
     """
+    # inputs
     subj_file = pjoin(work_dir, 'age_subj.csv')
     meas2file = {
         'myelin': '/nfs/e1/HCPD/fmriresults01/{sid}_V1_MR/'
@@ -51,11 +53,13 @@ def plot_box_violin(meas_name, roi_name):
                      '{sid}_V1_MR.thickness_MSMAll.32k_fs_LR.dscalar.nii'
     }
 
+    # prepare
     df = pd.read_csv(subj_file)
     atlas = Atlas('Cole_visual_LR')
     assert atlas.maps.shape == (1, LR_count_32k)
     roi_idx_map = atlas.maps[0] == atlas.roi2label[roi_name]
 
+    # plot
     points_list = []
     for subj_id in df['subID']:
         meas_file = meas2file[meas_name].format(sid=subj_id)
@@ -73,19 +77,93 @@ def plot_box_violin(meas_name, roi_name):
     plt.show()
 
 
-def plot_outlier_distribution(meas_name, roi_name):
+def find_outlier_per_subject_IQR(meas_name, roi_name, iqr_coef):
     """
-    统计在所有N个被试中，同时在至少x个被试中成为outliner的顶点数量
-    x属于range(1, N+1)
+    根据IQR，为所有被试找出左/右视觉系统的outliner
 
     Args:
         meas_name (str): thickness | myelin
         roi_name (str): L_cole_visual | R_cole_visual
+        iqr_coef (float):
+            set non-outlier in [Q1 - iqr_coefxIQR, Q3 + iqr_coefxIQR]
     """
+    # inputs
     info_file = '/nfs/e1/HCPD/HCPD_SubjInfo.csv'
+    meas2file = {
+        'myelin': '/nfs/e1/HCPD/fmriresults01/{sid}_V1_MR/'
+                  'MNINonLinear/fsaverage_LR32k/'
+                  '{sid}_V1_MR.MyelinMap_BC_MSMAll.32k_fs_LR.dscalar.nii',
+        'thickness': '/nfs/e1/HCPD/fmriresults01/{sid}_V1_MR/'
+                     'MNINonLinear/fsaverage_LR32k/'
+                     '{sid}_V1_MR.thickness_MSMAll.32k_fs_LR.dscalar.nii'
+    }
+
+    # outputs
+    out_file = pjoin(work_dir, f'{meas_name}_{roi_name}_{iqr_coef}IQR.npy')
+
+    # prepare
+    df = pd.read_csv(info_file)
+    n_subj = df.shape[0]
+    atlas = Atlas('Cole_visual_LR')
+    assert atlas.maps.shape == (1, LR_count_32k)
+    roi_idx_map = atlas.maps[0] == atlas.roi2label[roi_name]
+    n_vtx = np.sum(roi_idx_map)
+    data = np.zeros((n_subj, n_vtx), bool)
+
+    # calclate
+    for subj_idx, subj_id in enumerate(df['subID']):
+        time1 = time.time()
+        meas_file = meas2file[meas_name].format(sid=subj_id)
+        meas_map = nib.load(meas_file).get_fdata()[0]
+        meas_vec = meas_map[roi_idx_map]
+        Q1 = np.percentile(meas_vec, 25)
+        Q3 = np.percentile(meas_vec, 75)
+        IQR = Q3 - Q1
+        step = iqr_coef * IQR
+        data[subj_idx] = np.logical_or(meas_vec < Q1-step, meas_vec > Q3+step)
+        print(f'Finished: {subj_idx+1}/{n_subj},'
+              f'cost: {time.time() - time1} seconds.')
+
+    # save
+    np.save(out_file, data)
+
+
+def plot_outlier_distribution(fpath, title):
+    """
+    统计在所有N个被试中，同时在至少x个被试中成为outliner的顶点比例
+    x属于range(1, N+1)
+    """
+    data = np.load(fpath)
+    n_subj, n_vtx = data.shape
+    data = np.sum(data, axis=0)
+
+    x = np.arange(1, n_subj+1)
+    y = np.zeros(n_subj, np.float64)
+    for i, j in enumerate(x):
+        y[i] = np.sum(data >= j) / n_vtx
+    print('total vertices:', n_vtx)
+    plt.plot(x, y)
+    plt.title(title)
+    plt.xlabel('#subjects')
+    plt.ylabel('#vertices')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
     # select_subject()
-    plot_box_violin(meas_name='thickness', roi_name='R_cole_visual')
-    plot_box_violin(meas_name='myelin', roi_name='R_cole_visual')
+    # plot_box_violin(meas_name='thickness', roi_name='R_cole_visual')
+    # plot_box_violin(meas_name='myelin', roi_name='R_cole_visual')
+    # find_outlier_per_subject_IQR(meas_name='thickness', roi_name='R_cole_visual', iqr_coef=1.5)
+    # find_outlier_per_subject_IQR(meas_name='myelin', roi_name='R_cole_visual', iqr_coef=1.5)
+    plot_outlier_distribution(
+        fpath=pjoin(work_dir, 'thickness_R_cole_visual_1.5IQR.npy'),
+        title='thickness-R_cole_visual-1.5IQR'
+    )
+    plot_outlier_distribution(
+        fpath=pjoin(work_dir, 'myelin_R_cole_visual_1.5IQR.npy'),
+        title='myelin-R_cole_visual-1.5IQR'
+    )
