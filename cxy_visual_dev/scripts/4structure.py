af08ede69aa29a35f053df77d3ec7a8120be6971
sunshineDrizzle <954830460@qq.com>
2021-06-15 06:41:50 -0400

    cxy_visual_dev: do ROI analysis on PC1 for myelin and thickness and plot development line for each ROI

--- cxy_visual_dev/scripts/4structure.py
+++ cxy_visual_dev/scripts/4structure.py
@@ -1,4 +1,5 @@
 import os
+from re import A
 import numpy as np
 import pandas as pd
 import pickle as pkl
@@ -77,6 +78,45 @@ def pca(fpath, atlas_name, roi_name, n_component, out_csv, out_pkl):
     pkl.dump(pca, open(out_pkl, 'wb'))
 
 
+def ROI_analysis_on_PC(meas_file, pca_file, pc_num,
+                       mask_atlas, mask_name, roi_atlas, out_file):
+    """
+    利用指定PC的weights加权各ROI内的值
+    """
+    # prepare measure maps
+    meas_maps = nib.load(meas_file).get_fdata()
+    atlas_mask = Atlas(mask_atlas)
+    assert atlas_mask.maps.shape == (1, LR_count_32k)
+    roi_idx_map = atlas_mask.maps[0] == atlas_mask.roi2label[mask_name]
+    meas_maps = meas_maps[:, roi_idx_map]
+
+    # prepare ROI map
+    atlas_roi = Atlas(roi_atlas)
+    assert atlas_roi.maps.shape == (1, LR_count_32k)
+    roi_map = atlas_roi.maps[0][roi_idx_map]
+    roi_labels = np.unique(roi_map)
+    roi_label2name = {}
+    for name, lbl in atlas_roi.roi2label.items():
+        roi_label2name[lbl] = name
+
+    # prepare PCA
+    pca = pkl.load(open(pca_file, 'rb'))
+    eigen_vec = pca.components_.T[:, [pc_num-1]]
+
+    # calculate
+    meas_maps = meas_maps - np.expand_dims(pca.mean_, 0)
+    out_df = pd.DataFrame()
+    for lbl in roi_labels:
+        idx_map = roi_map == lbl
+        X = meas_maps[:, idx_map]
+        eigen_tmp = eigen_vec[idx_map]
+        y = np.matmul(X, eigen_tmp)
+        out_df[roi_label2name[lbl]] = y[:, 0]
+
+    # save
+    out_df.to_csv(out_file, index=False)
+
+
 if __name__ == '__main__':
     # HCP_MMP1 atlas 包含 Cole_visual_ROI
     # calc_TM(dataset_name='HCPD', meas_name='myelin', atlas_name='HCP_MMP1')
@@ -87,15 +127,29 @@ if __name__ == '__main__':
     # calc_TM(dataset_name='HCPD', meas_name='thickness', atlas_name='Cole_visual_LR')
     # calc_TM(dataset_name='HCPD', meas_name='myelin', atlas_name='HCP_MMP1', zscore_flag=True)
     # calc_TM(dataset_name='HCPD', meas_name='thickness', atlas_name='HCP_MMP1', zscore_flag=True)
-    pca(
-        fpath=pjoin(proj_dir, 'data/HCP/HCPD_thickness_4mm.dscalar.nii'),
-        atlas_name='Cole_visual_LR', roi_name='R_cole_visual', n_component=20,
-        out_csv=pjoin(work_dir, 'HCPD_thickness_4mm_PCA-20.csv'),
-        out_pkl=pjoin(work_dir, 'HCPD_thickness_4mm_PCA-20.pkl')
+    # pca(
+    #     fpath=pjoin(proj_dir, 'data/HCP/HCPD_thickness_4mm.dscalar.nii'),
+    #     atlas_name='Cole_visual_LR', roi_name='R_cole_visual', n_component=20,
+    #     out_csv=pjoin(work_dir, 'HCPD_thickness_4mm_R_cole_visual_PCA-20.csv'),
+    #     out_pkl=pjoin(work_dir, 'HCPD_thickness_4mm_R_cole_visual_PCA-20.pkl')
+    # )
+    # pca(
+    #     fpath=pjoin(proj_dir, 'data/HCP/HCPD_myelin_4mm.dscalar.nii'),
+    #     atlas_name='Cole_visual_LR', roi_name='R_cole_visual', n_component=20,
+    #     out_csv=pjoin(work_dir, 'HCPD_myelin_4mm_R_cole_visual_PCA-20.csv'),
+    #     out_pkl=pjoin(work_dir, 'HCPD_myelin_4mm_R_cole_visual_PCA-20.pkl')
+    # )
+    ROI_analysis_on_PC(
+        meas_file=pjoin(proj_dir, 'data/HCP/HCPD_thickness_4mm.dscalar.nii'),
+        pca_file=pjoin(work_dir, 'HCPD_thickness_4mm_R_cole_visual_PCA-20.pkl'),
+        pc_num=1, mask_atlas='Cole_visual_LR', mask_name='R_cole_visual',
+        roi_atlas='Cole_visual_ROI',
+        out_file=pjoin(work_dir, 'HCPD_thickness_4mm_R_cole_visual_ROI-PC1.csv')
     )
-    pca(
-        fpath=pjoin(proj_dir, 'data/HCP/HCPD_myelin_4mm.dscalar.nii'),
-        atlas_name='Cole_visual_LR', roi_name='R_cole_visual', n_component=20,
-        out_csv=pjoin(work_dir, 'HCPD_myelin_4mm_PCA-20.csv'),
-        out_pkl=pjoin(work_dir, 'HCPD_myelin_4mm_PCA-20.pkl')
+    ROI_analysis_on_PC(
+        meas_file=pjoin(proj_dir, 'data/HCP/HCPD_myelin_4mm.dscalar.nii'),
+        pca_file=pjoin(work_dir, 'HCPD_myelin_4mm_R_cole_visual_PCA-20.pkl'),
+        pc_num=1, mask_atlas='Cole_visual_LR', mask_name='R_cole_visual',
+        roi_atlas='Cole_visual_ROI',
+        out_file=pjoin(work_dir, 'HCPD_myelin_4mm_R_cole_visual_ROI-PC1.csv')
     )
