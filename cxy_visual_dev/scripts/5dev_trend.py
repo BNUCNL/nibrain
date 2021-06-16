87c81e1144e54dd0e7c676ee2b0a350522cfba1a
sunshineDrizzle <954830460@qq.com>
2021-06-16 05:03:12 -0400

    cxy_visual_dev: calc and plot kendall tau for each ROI on

--- cxy_visual_dev/scripts/5dev_trend.py
+++ cxy_visual_dev/scripts/5dev_trend.py
@@ -6,9 +6,8 @@ from os.path import join as pjoin
 from magicbox.io.io import CiftiReader, save2cifti
 from cxy_visual_dev.lib.predefine import Atlas, LR_count_32k,\
     mmp_map_file, mmp_name2label, get_parcel2label_by_ColeName,\
-    dataset_name2info
+    dataset_name2info, proj_dir
 
-proj_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/visual_dev'
 work_dir = pjoin(proj_dir, 'analysis/dev_trend')
 if not os.path.isdir(work_dir):
     os.makedirs(work_dir)
@@ -16,7 +15,7 @@ if not os.path.isdir(work_dir):
 
 def calc_mann_kendall(data_file, info_file, out_file):
     """
-    用 kendall tau刻画每个ROI的发育趋势
+    用 kendall tau刻画每个column的发育趋势
     """
     # load
     df = pd.read_csv(data_file)
@@ -25,15 +24,15 @@ def calc_mann_kendall(data_file, info_file, out_file):
     age_uniq = np.unique(ages)
 
     # calculate
-    out_df = pd.DataFrame(index=df.columns, columns=('tau', 'p'))
-    for idx in out_df.index:
-        meas_vec = np.array(df[idx])
+    out_df = pd.DataFrame(index=('tau', 'p'), columns=df.columns)
+    for col in out_df.columns:
+        meas_vec = np.array(df[col])
         y = np.zeros_like(age_uniq, dtype=np.float64)
         for age_idx, age in enumerate(age_uniq):
             y[age_idx] = np.mean(meas_vec[ages == age])
         mk_test = mk.original_test(y, 0.05)
-        out_df.loc[idx, 'tau'] = mk_test.Tau
-        out_df.loc[idx, 'p'] = mk_test.p
+        out_df.loc['tau', col] = mk_test.Tau
+        out_df.loc['p', col] = mk_test.p
 
     # save
     out_df.to_csv(out_file)
@@ -52,8 +51,8 @@ def kendall2cifti(data_file, rois, atlas_name, out_file):
     # calculate
     for roi in rois:
         mask = atlas.maps[0] == atlas.roi2label[roi]
-        out_data[0, mask] = df.loc[roi, 'tau']
-        out_data[1, mask] = -np.log10(df.loc[roi, 'p'])
+        out_data[0, mask] = df.loc['tau', roi]
+        out_data[1, mask] = -np.log10(df.loc['p', roi])
 
     # save
     save2cifti(out_file, out_data, CiftiReader(mmp_map_file).brain_models(),
@@ -72,6 +71,16 @@ if __name__ == '__main__':
         info_file=dataset_name2info['HCPD'],
         out_file=pjoin(work_dir, 'HCPD_thickness_HCP_MMP1_kendall.csv')
     )
+    calc_mann_kendall(
+        data_file=pjoin(proj_dir, 'analysis/structure/HCPD_myelin_4mm_R_cole_visual_ROI-PC1.csv'),
+        info_file=dataset_name2info['HCPD'],
+        out_file=pjoin(work_dir, 'HCPD_myelin_4mm_R_cole_visual_ROI-PC1_kendall.csv')
+    )
+    calc_mann_kendall(
+        data_file=pjoin(proj_dir, 'analysis/structure/HCPD_thickness_4mm_R_cole_visual_ROI-PC1.csv'),
+        info_file=dataset_name2info['HCPD'],
+        out_file=pjoin(work_dir, 'HCPD_thickness_4mm_R_cole_visual_ROI-PC1_kendall.csv')
+    )
 
     # map HCP_MMP1's kendall to cifti
     # rois = list(mmp_name2label.keys())
@@ -87,16 +96,16 @@ if __name__ == '__main__':
     # )
 
     # map Cole_visual_ROI's kendall to cifti
-    net_names = ['Primary Visual', 'Secondary Visual',
-                 'Posterior Multimodal', 'Ventral Multimodal']
-    rois = list(get_parcel2label_by_ColeName(net_names).keys())
-    kendall2cifti(
-        data_file=pjoin(work_dir, 'HCPD_myelin_HCP_MMP1_kendall.csv'),
-        rois=rois, atlas_name='Cole_visual_ROI',
-        out_file=pjoin(work_dir, 'HCPD_myelin_Cole_visual_ROI_kendall.dscalar.nii')
-    )
-    kendall2cifti(
-        data_file=pjoin(work_dir, 'HCPD_thickness_HCP_MMP1_kendall.csv'),
-        rois=rois, atlas_name='Cole_visual_ROI',
-        out_file=pjoin(work_dir, 'HCPD_thickness_Cole_visual_ROI_kendall.dscalar.nii')
-    )
+    # net_names = ['Primary Visual', 'Secondary Visual',
+    #              'Posterior Multimodal', 'Ventral Multimodal']
+    # rois = list(get_parcel2label_by_ColeName(net_names).keys())
+    # kendall2cifti(
+    #     data_file=pjoin(work_dir, 'HCPD_myelin_HCP_MMP1_kendall.csv'),
+    #     rois=rois, atlas_name='Cole_visual_ROI',
+    #     out_file=pjoin(work_dir, 'HCPD_myelin_Cole_visual_ROI_kendall.dscalar.nii')
+    # )
+    # kendall2cifti(
+    #     data_file=pjoin(work_dir, 'HCPD_thickness_HCP_MMP1_kendall.csv'),
+    #     rois=rois, atlas_name='Cole_visual_ROI',
+    #     out_file=pjoin(work_dir, 'HCPD_thickness_Cole_visual_ROI_kendall.dscalar.nii')
+    # )
