d169ea6d442208f50601fb1414ddf40da2157b44
masai1998 <202028061022@mail.bnu.edu.cn>
2021-06-30 20:46:07 +0800

    Update cb.m

--- cb_dev/ms/cb.m
+++ cb_dev/ms/cb.m
@@ -17,7 +17,7 @@ subject_id(strcmp(subject_id,'..'))=[];
 subject_id(strcmp(subject_id,'manifests'))=[];
 
 %%
-% prepare data using SUIT
+% caculate VBM & Myelin
 work_dir=fullfile('/nfs/e1/HCPD_CB/mri/');
 % for id=1:length(subject_id)
 for id=1:2
@@ -38,29 +38,37 @@ for id=1:2
     job.subjND.white={fullfile(work_dir,subject_id{id},'anat','T1w_seg2.nii')};
     job.subjND.isolation={fullfile(work_dir,subject_id{id},'anat','c_T1w_pcereb.nii')};
     suit_normalize_dartel(job)
-    % T2w
-    job.subjND.gray={fullfile(work_dir,subject_id{id},'anat','T2w_seg1.nii')};
-    job.subjND.white={fullfile(work_dir,subject_id{id},'anat','T2w_seg2.nii')};
-    job.subjND.isolation={fullfile(work_dir,subject_id{id},'anat','c_T2w_pcereb.nii')};
-    suit_normalize_dartel(job)
-    % reslicing
-    % T1w
+    % caculate VBM
     job.subj.affineTr={fullfile(work_dir,subject_id{id},'anat','Affine_T1w_seg1.mat')};
     job.subj.flowfield={fullfile(work_dir,subject_id{id},'anat','u_a_T1w_seg1.nii')};
     job.subj.resample={fullfile(work_dir,subject_id{id},'anat','T1w_seg1.nii')};
     job.subj.mask={fullfile(work_dir,subject_id{id},'anat','c_T1w_pcereb.nii')};
     job.jactransf=1;
     suit_reslice_dartel(job)
-    % T2w
-    job.subj.affineTr={fullfile(work_dir,subject_id{id},'anat','Affine_T2w_seg1.mat')};
-    job.subj.flowfield={fullfile(work_dir,subject_id{id},'anat','u_a_T2w_seg1.nii')};
-    job.subj.resample={fullfile(work_dir,subject_id{id},'anat','T2w_seg1.nii')};
-    job.subj.mask={fullfile(work_dir,subject_id{id},'anat','c_T2w_pcereb.nii')};
-    job.jactransf=1;
+    % caculate Myelin
+    % read T1w & T2w data
+    t1w_img=spm_vol(fullfile(work_dir,subject_id{id},'anat','T1w.nii'));
+    t2w_img=spm_vol(fullfile(work_dir,subject_id{id},'anat','T2w.nii'));
+    t1w_data=spm_read_vols(t1w_img);
+    t2w_data=spm_read_vols(t2w_img);
+    % T1w devided by T2w
+    myelin_data=t1w_data./t2w_data;
+    % save as nifti
+    t1w_img.fname=fullfile(work_dir,subject_id{id},'anat','myelin_map.nii');
+    spm_write_vol(t1w_img,myelin_data);
+    % resample to SUIT speace
+    job.subj.affineTr={fullfile(work_dir,subject_id{id},'anat','Affine_T1w_seg1.mat')};
+    job.subj.flowfield={fullfile(work_dir,subject_id{id},'anat','u_a_T1w_seg1.nii')};
+    job.subj.resample={fullfile(work_dir,subject_id{id},'anat','myelin_map.nii')};
+    job.subj.mask={fullfile(work_dir,subject_id{id},'anat','c_T1w_pcereb.nii')};
     suit_reslice_dartel(job)
+    % show flat map
+    map=suit_map2surf(fullfile(work_dir,subject_id{id},'anat','wdmyelin_map.nii'));
+    suit_plotflatmap(map)
     % delete native images
     delete(fullfile(work_dir,subject_id{id},'anat','T1w.nii'))
     delete(fullfile(work_dir,subject_id{id},'anat','T1w.nii.gz'))
     delete(fullfile(work_dir,subject_id{id},'anat','T2w.nii'))
     delete(fullfile(work_dir,subject_id{id},'anat','T2w.nii.gz'))
 end
+
