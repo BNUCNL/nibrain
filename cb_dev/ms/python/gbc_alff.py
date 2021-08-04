a3bccd1d1774a80383219e3ec3a80988e9fd05ab
masai1998 <202028061022@mail.bnu.edu.cn>
2021-08-04 19:24:04 +0800

    Update gbc_alff.py

--- cb_dev/ms/python/gbc_alff.py
+++ cb_dev/ms/python/gbc_alff.py
@@ -47,7 +47,7 @@ class CapAtlas(object):
 
         # if roi_list is None, it will be assigned all parcels
         if roi_list == None:
-            roi_list = self.map['KEYVALUE'].tolist()
+            roi_list = self.annot['KEYVALUE'].tolist()
 
         # get an empty array to store the parcels mask
         parcel_mask = np.zeros((len(roi_list), self.map.shape[1]))
@@ -56,15 +56,15 @@ class CapAtlas(object):
             roi_keyvalue = self.annot[self.annot['NETWORKKEY'] == roi_id]['KEYVALUE']
             parcel_mask[roi_index, :] = (self.map[0, :] == np.array(roi_keyvalue)[:, None]).any(axis=0).astype(bool)
 
-        return parcel_mask
+        return parcel_mask.astype(bool)
 
-    def get_network(self, hemisphere):
+    def get_network(self, hemisphere=None):
         '''
         Get logical array mask of CAP networks
 
         Args:
             hemisphere: str
-                To determine the hemisphere you want, enter "L", "R" or "LR"
+                To determine the hemisphere you want, enter "L", "R", "LR" or None
 
         Returns:
             network_mask: numpy array
@@ -72,17 +72,23 @@ class CapAtlas(object):
         '''
 
         # get an empty array to store the networks mask
-        network_mask = np.zeros((self.annot['NETWORKKEY'].max() * len(hemisphere), self.map.shape[1]))
+        network_mask = np.zeros((self.annot['NETWORKKEY'].max(), self.map.shape[1]))
+
         # get the mask according to the input hemisphere
-        for hemi in list(hemisphere):
-            annot_hemi = self.annot[self.annot['LABEL'].str.contains(hemi + '-')]
+        if hemisphere == 'L' or hemisphere == 'R':
+            annot_hemi = self.annot[self.annot['LABEL'].str.contains(hemisphere + '-')]
             # loop the array of network-id to get the mask of each element
             for network_id in np.arange(1, annot_hemi['NETWORKKEY'].max() + 1):
                 network_keyvalue = annot_hemi[annot_hemi['NETWORKKEY'] == network_id]['KEYVALUE']
                 network_mask[network_id - 1, :] = (self.map[0, :] == np.array(network_keyvalue)[:, None]).any(
                     axis=0).astype(bool)
+        else:
+            for network_id in np.arange(1, self.annot['NETWORKKEY'].max() + 1):
+                network_keyvalue = self.annot[self.annot['NETWORKKEY'] == network_id]['KEYVALUE']
+                network_mask[network_id - 1, :] = (self.map[0, :] == np.array(network_keyvalue)[:, None]).any(
+                    axis=0).astype(bool)
 
-        return network_mask
+        return network_mask.astype(bool)
 
     def get_cortex(self, hemisphere):
         '''
@@ -106,7 +112,11 @@ class CapAtlas(object):
             cortex_keyvalue = annot_hemi[annot_hemi['LABEL'].str.endswith('Ctx')]['KEYVALUE']
             cortex_mask[index, :] = (self.map[0, :] == np.array(cortex_keyvalue)[:, None]).any(axis=0).astype(bool)
 
-        return cortex_mask
+        # if shape is (1, 91282), convert it to (91282,)
+        if cortex_mask.shape[0] == 1:
+            cortex_mask = np.squeeze(cortex_mask)
+
+        return cortex_mask.astype(bool)
 
     def get_subcortex(self, hemisphere):
         '''
@@ -133,7 +143,11 @@ class CapAtlas(object):
             subcortex_mask[index, :] = (self.map[0, :] == np.array(subcortex_keyvalue)[:, None]).any(axis=0).astype(
                 bool)
 
-        return subcortex_mask
+        # if shape is (1, 91282), convert it to (91282,)
+        if subcortex_mask.shape[0] == 1:
+            subcortex_mask = np.squeeze(subcortex_mask)
+
+        return subcortex_mask.astype(bool)
 
     def get_cerebellum(self, hemisphere):
         '''
@@ -158,7 +172,11 @@ class CapAtlas(object):
             cerebellum_mask[index, :] = (self.map[0, :] == np.array(cerebellum_keyvalue)[:, None]).any(axis=0).astype(
                 bool)
 
-        return cerebellum_mask
+        # if shape is (1, 91282), convert it to (91282,)
+        if cerebellum_mask.shape[0] == 1:
+            cerebellum_mask = np.squeeze(cerebellum_mask)
+
+        return cerebellum_mask.astype(bool)
 
     def get_brainstem(self):
         '''
@@ -170,12 +188,12 @@ class CapAtlas(object):
         '''
 
         # get an empty array to store the brainstem mask
-        brainstem_mask = np.zeros((1, self.map.shape[1]))
+        brainstem_mask = np.zeros((self.map.shape[1], None))
         # get the id of brainstem ROI from annotation dataframe
         brainstem_keyvalue = self.annot[self.annot['LABEL'].str.endswith('Brainstem')]['KEYVALUE']
-        brainstem_mask[0, :] = (self.map[0, :] == np.array(brainstem_keyvalue)[:, None]).any(axis=0).astype(bool)
+        brainstem_mask[:, None] = (self.map[0, :] == np.array(brainstem_keyvalue)[:, None]).any(axis=0).astype(bool)
 
-        return brainstem_mask
+        return brainstem_mask.astype(bool)
 
 
 def global_brain_conn(cifti_ts, src_roi, targ_roi):
@@ -196,10 +214,9 @@ def global_brain_conn(cifti_ts, src_roi, targ_roi):
     '''
 
     # prepare source and target data index with src_roi & targ_roi
-    cifti_matrix = cifti_ts.get_fdata(structure=None)
+    cifti_matrix = cifti_ts.get_fdata()
     src_matrix = cifti_matrix[:, src_roi]
     targ_matrix = cifti_matrix[:, targ_roi]
-    targ_matrix = ~targ_matrix & src_matrix
 
     # calculate mean of connectivity between source and target
     source_brain_conn = 1 - cdist(src_matrix.T, targ_matrix.T, metric='correlation')
@@ -208,7 +225,7 @@ def global_brain_conn(cifti_ts, src_roi, targ_roi):
     return conn_mean
 
 
-def alff(cifti_ts, src_roi, tr, low_freq_band=(0.01, 0.1), type=None):
+def alff(cifti_ts, src_roi, tr, low_freq_band=(0.01, 0.1)):
     '''
     Calculate Amplitude of low-frequency fluctuation (ALFF) or Fractional amplitude of low-frequency fluctuation (fALFF) of source ROI
 
@@ -221,19 +238,14 @@ def alff(cifti_ts, src_roi, tr, low_freq_band=(0.01, 0.1), type=None):
             Repetation time (second)
         low_freq_band: tuple
             (m, n) low freqency scale is from m to n (Hz)
-        return_type: str
-            None: return a tuple of alff and falff
-            ALFF/fALFF: return ALFF/fALFF
 
     Returns:
-        alff: numpy array
-            ALFF of source ROI
-        falff: numpy array
-            fALFF of source ROI
+        alff_falff: tuple
+            ALFF & fALFF numpy array of source ROI ()
     '''
 
     # prepare source data index with src_roi
-    cifti_matrix = cifti_ts.get_fdata(structure=None)
+    cifti_matrix = cifti_ts.get_fdata()
     src_matrix = cifti_matrix[:, src_roi]
 
     # fast fourier transform
@@ -245,17 +257,13 @@ def alff(cifti_ts, src_roi, tr, low_freq_band=(0.01, 0.1), type=None):
     # calculate ALFF or fALFF
     # ALFF: sum of low band power
     # fALFF: ratio of Alff to total power
-    time_half = cifti_matrix.shape[0] / 2
-    alff = np.sum(total_power[(low_freq_band[0] <= freq_scale)[0:time_half] & (freq_scale <= low_freq_band[1])[0:time_half],:], axis=0)
-    if type == 'ALFF':
-        return alff
-    if type == 'fALFF':
-        falff = alff / np.sum(total_power, axis=0)
-        return falff
-    if type == None:
-        falff = alff / np.sum(total_power, axis=0)
-        return tuple(alff, falff)
+    time_half = int(cifti_matrix.shape[0] / 2)
+    alff = np.sum(
+        total_power[(low_freq_band[0] <= freq_scale)[0:time_half] & (freq_scale <= low_freq_band[1])[0:time_half], :],
+        axis=0)
+    falff = alff / np.sum(total_power, axis=0)
 
+    return np.concatenate((alff.reshape(-1, 1), falff.reshape(-1, 1)), axis=1)
 
 def Ciftiwrite(file_path, data, cifti_ts, src_roi):
     '''
@@ -269,11 +277,11 @@ def Ciftiwrite(file_path, data, cifti_ts, src_roi):
         cifti_ts: nibabel cifti2Image
             time series object read by nibabel.load()
     '''
-    
+
     # get an empty array to store the restored source data
-    save_data = np.zeros(1, src_roi.shape[0])
+    save_data = np.zeros(data.shape[1], src_roi.shape[0])
     # restore source data to standard cifti space
-    save_data[0, src_roi] = data
+    save_data[:, src_roi] = data
     # save as cifti file
     img = nib.Cifti2Image(dataobj=save_data, header=cifti_ts.header)
     nib.cifti2.save(img, file_path)
