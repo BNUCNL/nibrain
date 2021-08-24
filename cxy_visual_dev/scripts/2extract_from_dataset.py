59ecd38a9d21006f7285440c14adf8b693047311
sunshineDrizzle <954830460@qq.com>
2021-08-24 09:29:16 -0400

    cxy_visual_dev: calculate ColeParcel_fc_vtx

--- cxy_visual_dev/scripts/2extract_from_dataset.py
+++ cxy_visual_dev/scripts/2extract_from_dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import nibabel as nib
 from os.path import join as pjoin
+from scipy.spatial.distance import cdist
 from magicbox.io.io import CiftiReader, save2cifti
 from cxy_visual_dev.lib.predefine import LR_count_32k,\
     mmp_map_file, dataset_name2dir, dataset_name2info,\
@@ -234,6 +235,66 @@ def alff(subj_par, subj_ids, stem_path, base_path, tr,
     save2cifti(falff_all_file, falff_all, brain_models, subj_ids, volume)
 
 
+def ColeParcel_fc_vtx(subj_par, subj_ids, stem_path, base_path):
+
+    # load atlas
+    cap_file = '/nfs/z1/atlas/ColeAnticevicNetPartition/' \
+               'CortexSubcortex_ColeAnticevic_NetPartition_wSubcorGSR_parcels_LR.dlabel.nii'
+    cap_LabelKey_file = '/nfs/z1/atlas/ColeAnticevicNetPartition/' \
+                        'CortexSubcortex_ColeAnticevic_NetPartition_wSubcorGSR_parcels_LR_LabelKey.txt'
+    map = nib.load(cap_file).get_fdata()[0]
+    df = pd.read_csv(cap_LabelKey_file, sep='\t', usecols=['KEYVALUE', 'LABEL'])
+    n_parcel = df.shape[0]
+
+    # start
+    n_subj = len(subj_ids)
+    first_flag = True
+    brain_models = None
+    volume = None
+    for subj_idx, subj_id in enumerate(subj_ids):
+        time1 = time.time()
+
+        # prepare path
+        stem_dir = pjoin(subj_par, subj_id, stem_path)
+        fpath_ = pjoin(stem_dir, base_path)
+        fpaths = glob.glob(fpath_)
+        n_run = len(fpaths)
+        if n_run == 0:
+            continue
+        out_file = pjoin(stem_dir, 'rsfc_ColeParcel2Vertex.dscalar.nii')
+
+        # loop all runs
+        fc_sub = np.zeros((n_parcel, All_count_32k), dtype=np.float64)
+        for run_idx, fpath in enumerate(fpaths):
+
+            # get data
+            if first_flag:
+                reader = CiftiReader(fpath)
+                brain_models = reader.brain_models()
+                volume = reader.volume
+                data = reader.get_data()
+                first_flag = False
+            else:
+                data = nib.load(fpath).get_fdata()
+            data = data.T
+            assert data.shape[0] == All_count_32k
+
+            # prepare ROI time series
+            data_roi = np.zeros((n_parcel, data.shape[1]), dtype=np.float64)
+            for idx, k in enumerate(df['KEYVALUE']):
+                data_roi[idx] = np.mean(data[map == k], 0)
+
+            # calculate RSFC
+            fc_run = 1 - cdist(data_roi, data, metric='correlation')
+            fc_sub += fc_run
+            print(f'Finish subj-{subj_idx+1}/{n_subj}_run-{run_idx+1}/{n_run}')
+
+        fc_sub /= n_run
+        save2cifti(out_file, fc_sub, brain_models, df['LABEL'], volume)
+        print(f'Finish subj-{subj_idx+1}/{n_subj}, '
+              f'cost {time.time()-time1} seconds')
+
+
 if __name__ == '__main__':
     # merge_data(dataset_name='HCPD', meas_name='thickness')
     # merge_data(dataset_name='HCPD', meas_name='myelin')
@@ -264,12 +325,21 @@ if __name__ == '__main__':
     #     tr=0.8, low_freq_band=(0.008, 0.1), linear_detrend=True
     # )
 
-    subj_par = '/nfs/z1/HCP/HCPA/fmriresults01'
-    subj_ids = sorted([i for i in os.listdir(subj_par) if i.startswith('HCA')])
-    alff(
+    # subj_par = '/nfs/z1/HCP/HCPA/fmriresults01'
+    # subj_ids = sorted([i for i in os.listdir(subj_par) if i.startswith('HCA')])
+    # alff(
+    #     subj_par=subj_par, subj_ids=subj_ids,
+    #     stem_path='MNINonLinear/Results',
+    #     base_path='rfMRI_REST?_??/'
+    #               'rfMRI_REST?_??_Atlas_MSMAll_hp0_clean.dtseries.nii',
+    #     tr=0.8, low_freq_band=(0.008, 0.1), linear_detrend=True
+    # )
+
+    subj_par = '/nfs/z1/HCP/HCPD/fmriresults01'
+    subj_ids = sorted([i for i in os.listdir(subj_par) if i.startswith('HCD')])
+    ColeParcel_fc_vtx(
         subj_par=subj_par, subj_ids=subj_ids,
         stem_path='MNINonLinear/Results',
         base_path='rfMRI_REST?_??/'
-                  'rfMRI_REST?_??_Atlas_MSMAll_hp0_clean.dtseries.nii',
-        tr=0.8, low_freq_band=(0.008, 0.1), linear_detrend=True
+                  'rfMRI_REST?_??_Atlas_MSMAll_hp0_clean.dtseries.nii'
     )
