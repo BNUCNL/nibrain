a3bc700bba6816432e7be3c5b83f6a84665b5efe
sunshineDrizzle <954830460@qq.com>
2021-09-14 02:28:43 -0400

    cxy_hcp_ffa: prepare 3factors-mix ANOVA for RSFC

--- cxy_hcp_ffa/scripts/grouping_rfMRI.py
+++ cxy_hcp_ffa/scripts/grouping_rfMRI.py
@@ -40,6 +40,59 @@ def pre_ANOVA_3factors():
     out_df.to_csv(trg_file, index=False)
 
 
+def pre_ANOVA_3factors_mix():
+    """
+    准备好3因素混合设计方差分析需要的数据。
+    被试间因子：group
+    被试内因子：hemisphere，ROI
+    2 groups x 2 hemispheres x 2 ROIs
+    """
+    import numpy as np
+    import pandas as pd
+    import pickle as pkl
+
+    gids = (1, 2)
+    hemis = ('lh', 'rh')
+    rois = ('pFus-face', 'mFus-face')
+    src_file = pjoin(anal_dir, 'rfMRI/rsfc_individual2Cole_{hemi}.pkl')
+    gid_file = pjoin(anal_dir, 'grouping/group_id_{hemi}_v2.npy')
+    trg_file = pjoin(work_dir, 'rsfc_individual2Cole_preANOVA-3factor-mix.csv')
+
+    hemi2data = {}
+    hemi2gids = {}
+    for hemi in hemis:
+        hemi2data[hemi] = pkl.load(open(src_file.format(hemi=hemi), 'rb'))
+        hemi2gids[hemi] = np.load(gid_file.format(hemi=hemi))
+
+    out_dict = {'gid': []}
+    for idx, gid in enumerate(gids):
+        gid_idx_vec = np.logical_and(hemi2gids['lh'] == gid,
+                                     hemi2gids['rh'] == gid)
+        nan_vec = None
+        for hemi in hemis:
+            data = hemi2data[hemi]
+            for roi in rois:
+                meas_vec = np.mean(data[roi][gid_idx_vec], 1)
+
+                if nan_vec is None:
+                    nan_vec = np.isnan(meas_vec)
+                    non_nan_vec = ~nan_vec
+                    n_valid = np.sum(non_nan_vec)
+                    print('#NAN:', np.sum(nan_vec))
+                else:
+                    assert np.all(nan_vec == np.isnan(meas_vec))
+
+                meas_vec = meas_vec[non_nan_vec]
+                if idx == 0:
+                    out_dict[f"{hemi}_{roi.split('-')[0]}"] = meas_vec.tolist()
+                else:
+                    out_dict[f"{hemi}_{roi.split('-')[0]}"].extend(meas_vec)
+        print(f'G{gid}:', n_valid)
+        out_dict['gid'].extend([gid] * n_valid)
+    out_df = pd.DataFrame(out_dict)
+    out_df.to_csv(trg_file, index=False)
+
+
 def roi_ttest(gid, trg_name2label, trg_labels=None):
     """
     compare rsfc difference between ROIs
@@ -187,6 +240,7 @@ def multitest_correct_ttest(gid=1):
     data.to_csv(out_file, index=False)
 
 
+# ---old---
 def prepare_plot(gid=1, hemi='lh'):
     import numpy as np
     import pickle as pkl
@@ -236,15 +290,16 @@ def prepare_plot(gid=1, hemi='lh'):
 
 if __name__ == '__main__':
     # pre_ANOVA_3factors()
-    roi_ttest(gid=1, trg_name2label=net2label_cole)
-    roi_ttest(gid=2, trg_name2label=net2label_cole)
-    roi_pair_ttest(gid=1, trg_name2label=net2label_cole)
-    roi_pair_ttest(gid=2, trg_name2label=net2label_cole)
-    multitest_correct_ttest(gid=1)
-    multitest_correct_ttest(gid=2)
+    pre_ANOVA_3factors_mix()
+    # roi_ttest(gid=1, trg_name2label=net2label_cole)
+    # roi_ttest(gid=2, trg_name2label=net2label_cole)
+    # roi_pair_ttest(gid=1, trg_name2label=net2label_cole)
+    # roi_pair_ttest(gid=2, trg_name2label=net2label_cole)
+    # multitest_correct_ttest(gid=1)
+    # multitest_correct_ttest(gid=2)
+
+    # old
     # prepare_plot(gid=1, hemi='lh')
     # prepare_plot(gid=1, hemi='rh')
     # prepare_plot(gid=2, hemi='lh')
     # prepare_plot(gid=2, hemi='rh')
-    # multitest_correct_ttest(gid=1)
-    # multitest_correct_ttest(gid=2)
