da55a09c5a79e02247882e7948be4fc110f0ebe8
sunshineDrizzle <954830460@qq.com>
2021-09-14 02:21:20 -0400

    cxy_hcp_ffa: prepare 3factors(-mix) ANOVA for face selectivity

--- cxy_hcp_ffa/scripts/tfMRI.py
+++ cxy_hcp_ffa/scripts/tfMRI.py
@@ -1,6 +1,6 @@
 from os.path import join as pjoin
+from cxy_hcp_ffa.lib.predefine import proj_dir
 
-proj_dir = '/nfs/t3/workingshop/chenxiayu/study/FFA_pattern'
 anal_dir = pjoin(proj_dir, 'analysis/s2/1080_fROI/refined_with_Kevin')
 work_dir = pjoin(anal_dir, 'tfMRI')
 
@@ -87,45 +87,8 @@ def calc_meas_emotion(hemi='lh'):
     pkl.dump(roi_meas, open(out_file, 'wb'))
 
 
-def pre_ANOVA_3factors():
-    """
-    准备好3因素被试间设计方差分析需要的数据。
-    2 hemispheres x 2 groups x 2 ROIs
-    """
-    import numpy as np
-    import pandas as pd
-    import pickle as pkl
-
-    gids = (1, 2)
-    hemis = ('lh', 'rh')
-    rois = ('pFus-face', 'mFus-face')
-    src_file = pjoin(work_dir, 'individual_activ_{hemi}.pkl')
-    gid_file = pjoin(anal_dir, 'grouping/group_id_{hemi}_v2_merged.npy')
-    trg_file = pjoin(work_dir, 'individual_activ_preANOVA-3factor.csv')
-
-    out_dict = {'gid': [], 'hemi': [], 'roi': [], 'meas': []}
-    for hemi in hemis:
-        data = pkl.load(open(src_file.format(hemi=hemi), 'rb'))
-        gid_vec = np.load(gid_file.format(hemi=hemi))
-        for gid in gids:
-            gid_vec_idx = gid_vec == gid
-            for roi in rois:
-                roi_idx = data['roi'].index(roi)
-                meas_vec = data['meas'][roi_idx][gid_vec_idx]
-                meas_vec = meas_vec[~np.isnan(meas_vec)]
-                n_valid = len(meas_vec)
-                out_dict['hemi'].extend([hemi] * n_valid)
-                out_dict['gid'].extend([gid] * n_valid)
-                out_dict['roi'].extend([roi.split('-')[0]] * n_valid)
-                out_dict['meas'].extend(meas_vec)
-                print(f'{hemi}_{gid}_{roi}:', n_valid)
-    out_df = pd.DataFrame(out_dict)
-    out_df.to_csv(trg_file, index=False)
-
-
 if __name__ == '__main__':
     # calc_meas_individual(hemi='lh')
     # calc_meas_individual(hemi='rh')
     # calc_meas_emotion(hemi='lh')
     # calc_meas_emotion(hemi='rh')
-    pre_ANOVA_3factors()
