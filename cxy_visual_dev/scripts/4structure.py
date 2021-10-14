1c7e5c29619e76703564257b8d2b0ecb8f656219
sunshineDrizzle <954830460@qq.com>
2021-10-14 12:32:05 -0400

    cxy_visual_dev: plot PCA-subj results of HCPY-MTAG

--- cxy_visual_dev/scripts/4structure.py
+++ cxy_visual_dev/scripts/4structure.py
@@ -7,7 +7,7 @@ from cxy_visual_dev.lib.algo import ROI_analysis, pca,\
     ROI_analysis_on_PC, make_age_maps, calc_map_corr,\
     mask_maps, merge_by_age, vtx_corr_col, polyfit, row_corr_row,\
     col_operate_col, map_operate_map, zscore_map, concate_map,\
-    zscore_map_subj, pca_mf, ROI_scalar
+    zscore_map_subj, ROI_scalar
 
 work_dir = pjoin(proj_dir, 'analysis/structure')
 if not os.path.isdir(work_dir):
@@ -95,26 +95,6 @@ if __name__ == '__main__':
     #     n_component=20, axis='subject',
     #     out_name=pjoin(work_dir, 'HCPD_thickness_4mm_R_cole_visual_PCA-subj')
     # )
-
-    pca_mf(
-        data_files=[
-            s1200_1096_myelin, s1200_1096_thickness,
-            pjoin(proj_dir, 'data/HCP/HCPY-alff.dscalar.nii'),
-            pjoin(proj_dir, 'data/HCP/HCPY-GBC_MMP-vis2.dscalar.nii')
-        ],
-        atlas_names=['MMP-vis2-LR', 'MMP-vis2-LR'],
-        roi_names=['L_MMP_vis2', 'R_MMP_vis2'],
-        n_component=20, axis='subject', zscore0=None, zscore1='split',
-        csv_files=[
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_M.csv'),
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_T.csv'),
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_A.csv'),
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_G.csv')
-        ],
-        cii_file=pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.dscalar.nii'),
-        pkl_file=pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.pkl'),
-        random_state=7
-    )
 
     # ROI_analysis_on_PC(
     #     data_file=pjoin(proj_dir, 'data/HCP/HCPD_thickness_4mm.dscalar.nii'),
