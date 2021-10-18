8b69004a9e7c4253478c652bea597e4929420cfb
sunshineDrizzle <954830460@qq.com>
2021-10-18 03:59:40 -0400

    cxy_visual_dev: calc and plot HCPY-M+T spatial PCA

--- cxy_visual_dev/scripts/calc_PCA.py
+++ cxy_visual_dev/scripts/calc_PCA.py
@@ -10,22 +10,36 @@ if not os.path.isdir(work_dir):
 
 
 if __name__ == '__main__':
+    # pca_mf(
+    #     data_files=[
+    #         s1200_1096_myelin, s1200_1096_thickness,
+    #         pjoin(proj_dir, 'data/HCP/HCPY-alff.dscalar.nii'),
+    #         pjoin(proj_dir, 'data/HCP/HCPY-GBC_MMP-vis2.dscalar.nii')
+    #     ],
+    #     atlas_names=['MMP-vis2-LR', 'MMP-vis2-LR'],
+    #     roi_names=['L_MMP_vis2', 'R_MMP_vis2'],
+    #     n_component=20, axis='subject', zscore0=None, zscore1='split',
+    #     csv_files=[
+    #         pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_M.csv'),
+    #         pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_T.csv'),
+    #         pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_A.csv'),
+    #         pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_G.csv')
+    #     ],
+    #     cii_file=pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.dscalar.nii'),
+    #     pkl_file=pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.pkl'),
+    #     random_state=7
+    # )
+
     pca_mf(
-        data_files=[
-            s1200_1096_myelin, s1200_1096_thickness,
-            pjoin(proj_dir, 'data/HCP/HCPY-alff.dscalar.nii'),
-            pjoin(proj_dir, 'data/HCP/HCPY-GBC_MMP-vis2.dscalar.nii')
-        ],
+        data_files=[s1200_1096_myelin, s1200_1096_thickness],
         atlas_names=['MMP-vis2-LR', 'MMP-vis2-LR'],
         roi_names=['L_MMP_vis2', 'R_MMP_vis2'],
         n_component=20, axis='subject', zscore0=None, zscore1='split',
         csv_files=[
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_M.csv'),
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_T.csv'),
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_A.csv'),
-            pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_G.csv')
+            pjoin(work_dir, 'HCPY-M+T_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_M.csv'),
+            pjoin(work_dir, 'HCPY-M+T_mask-L+R_MMP_vis2_zscore1-split_PCA-subj_T.csv')
         ],
-        cii_file=pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.dscalar.nii'),
-        pkl_file=pjoin(work_dir, 'HCPY-M+T+A+G_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.pkl'),
+        cii_file=pjoin(work_dir, 'HCPY-M+T_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.dscalar.nii'),
+        pkl_file=pjoin(work_dir, 'HCPY-M+T_mask-L+R_MMP_vis2_zscore1-split_PCA-subj.pkl'),
         random_state=7
     )
