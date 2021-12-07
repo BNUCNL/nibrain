b4c322e038ea8554bc04370ad1ad93f29972c59b
sunshineDrizzle <954830460@qq.com>
2021-12-07 08:53:40 -0500

    cxy_visual_dev: HCPA fit PC1+2

--- cxy_visual_dev/scripts/calc_fit.py
+++ cxy_visual_dev/scripts/calc_fit.py
@@ -1,8 +1,9 @@
 import os
+import numpy as np
 import nibabel as nib
 from os.path import join as pjoin
 from cxy_visual_dev.lib.predefine import proj_dir, Atlas,\
-    get_rois
+    get_rois, hemi2Hemi
 from cxy_visual_dev.lib.algo import cat_data_from_cifti,\
     linear_fit1
 
@@ -62,39 +63,60 @@ def old_fit():
     )
 
 
-if __name__ == '__main__':
-    mask = Atlas('HCP-MMP').get_mask(get_rois('MMP-vis3-R'))[0]
-    C1C2_maps = nib.load(pjoin(
-        anal_dir, 'decomposition/HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj.dscalar.nii'
-    )).get_fdata()[:2]
-
+def gdist_fit_PC1():
+    """
+    分别用枕极和MT做锚点，和距状沟锚点一起描述C1
+    这里用这三个map作为三个特征去线性拟合C1 (半脑)
+    """
+    Hemi = 'R'
+    mask = Atlas('HCP-MMP').get_mask(get_rois(f'MMP-vis3-{Hemi}'))[0]
+    pc_file = pjoin(
+        anal_dir, f'decomposition/HCPY-M+T_MMP-vis3-{Hemi}_zscore1_PCA-subj.dscalar.nii')
     src_files = [
             # pjoin(anal_dir, 'gdist/gdist_src-CalcarineSulcus.dscalar.nii'),
             pjoin(anal_dir, 'gdist/gdist_src-OccipitalPole.dscalar.nii'),
             pjoin(anal_dir, 'gdist/gdist_src-MT.dscalar.nii')]
+    feat_names = ['OccipitalPole', 'MT']
+    out_file = pjoin(work_dir, 'OcPole+MT=C1.csv')
+
+    C1_map = nib.load(pc_file).get_fdata()[0]
     X_list = []
     for src_file in src_files:
         data = nib.load(src_file).get_fdata()[:, mask]
         X_list.append(data.T)
-    Y = C1C2_maps[0, mask][:, None]
+    Y = np.expand_dims(C1_map[mask], 1)
     linear_fit1(
-        X_list=X_list, feat_names=['OccipitalPole', 'MT'],
+        X_list=X_list, feat_names=feat_names,
         Y=Y, trg_names=['C1'], score_metric='R2',
-        out_file=pjoin(work_dir, 'OcPole+MT=C1.csv'),
-        standard_scale=True
-    )
+        out_file=out_file, standard_scale=True)
 
-    # src_files = [
-    #         pjoin(proj_dir, 'data/HCP/HCPD_myelin.dscalar.nii'),
-    #         pjoin(proj_dir, 'data/HCP/HCPD_thickness.dscalar.nii')]
-    # X_list = []
-    # for src_file in src_files:
-    #     data = nib.load(src_file).get_fdata()[:, mask]
-    #     X_list.append(data.T)
-    # Y = C1C2_maps[:, mask].T
-    # linear_fit1(
-    #     X_list=X_list, feat_names=['Myelination', 'Thickness'],
-    #     Y=Y, trg_names=['C1', 'C2'], score_metric='R2',
-    #     out_file=pjoin(work_dir, 'HCPD-M+T=C1C2.csv'),
-    #     standard_scale=True
-    # )
+
+def HCPDA_fit_PC12():
+    """
+    用HCPD/A每个被试的myelin和thickness map去拟合C1C2（半脑）
+    """
+    Hemi = 'R'
+    mask = Atlas('HCP-MMP').get_mask(get_rois(f'MMP-vis3-{Hemi}'))[0]
+    pc_file = pjoin(
+        anal_dir, f'decomposition/HCPY-M+T_MMP-vis3-{Hemi}_zscore1_PCA-subj.dscalar.nii')
+    src_files = [
+            pjoin(proj_dir, 'data/HCP/HCPA_myelin.dscalar.nii'),
+            pjoin(proj_dir, 'data/HCP/HCPA_thickness.dscalar.nii')]
+    feat_names = ['Myelination', 'Thickness']
+    out_file = pjoin(work_dir, 'HCPA-M+T=C1C2.csv')
+
+    C1C2_maps = nib.load(pc_file).get_fdata()[:2]
+    X_list = []
+    for src_file in src_files:
+        data = nib.load(src_file).get_fdata()[:, mask]
+        X_list.append(data.T)
+    Y = C1C2_maps[:, mask].T
+    linear_fit1(
+        X_list=X_list, feat_names=feat_names,
+        Y=Y, trg_names=['C1', 'C2'], score_metric='R2',
+        out_file=out_file, standard_scale=True)
+
+
+if __name__ == '__main__':
+    # gdist_fit_PC1()
+    HCPDA_fit_PC12()
