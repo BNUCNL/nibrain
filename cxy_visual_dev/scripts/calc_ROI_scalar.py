9450ea244c1d8a382871ecf2d86b42ef26d390ce
sunshineDrizzle <954830460@qq.com>
2021-12-29 01:41:25 -0500

    cxy_visual_dev: update ROI_scalar

--- cxy_visual_dev/scripts/calc_ROI_scalar.py
+++ cxy_visual_dev/scripts/calc_ROI_scalar.py
@@ -1,9 +1,8 @@
 import os
 import pandas as pd
-import nibabel as nib
 from os.path import join as pjoin
 from magicbox.algorithm.array import summary_across_col_by_mask
-from cxy_visual_dev.lib.predefine import get_rois, proj_dir, Atlas
+from cxy_visual_dev.lib.predefine import proj_dir, Atlas
 from magicbox.io.io import CiftiReader
 
 anal_dir = pjoin(proj_dir, 'analysis')
@@ -12,18 +11,18 @@ if not os.path.isdir(work_dir):
     os.makedirs(work_dir)
 
 
-def ROI_scalar(src_file, atlas, metric, out_file, rois='all', out_index=None):
+def ROI_scalar(src_file, mask, values, metric, out_file, rois=None, out_index=None):
     """
     为每个被试每个ROI求scalar value
 
     Args:
         src_file (str): end with .dscalar.nii
             shape=(n_map, n_vtx)
-        atlas (Atlas): include ROIs' labels and mask map
+        mask (1D array): mask map
+        values (sequence):
         metric (str): mean, sum, sem
         out_file (str): end with .csv
-        rois (str | strings): ROI names
-            If is str, must be 'all'.
+        rois (strings): ROI names corresponding to values
         out_index (None | str | sequence):
             If None, don't save index to out_file.
             If str, must be 'map_name' that means using map names as indices.
@@ -31,16 +30,10 @@ def ROI_scalar(src_file, atlas, metric, out_file, rois='all', out_index=None):
     """
     reader = CiftiReader(src_file)
     src_maps = reader.get_data()
+    columns = values if rois is None else rois
 
-    if rois == 'all':
-        rois = list(atlas.roi2label.keys())
-        values = list(atlas.roi2label.values())
-    else:
-        values = [atlas.roi2label[i] for i in rois]
-
-    out_data = summary_across_col_by_mask(
-        src_maps, atlas.maps[0], values, metric)
-    out_df = pd.DataFrame(out_data, columns=rois)
+    out_data = summary_across_col_by_mask(src_maps, mask, values, metric)
+    out_df = pd.DataFrame(out_data, columns=columns)
 
     if out_index is None:
         out_df.to_csv(out_file, index=False)
@@ -54,34 +47,10 @@ def ROI_scalar(src_file, atlas, metric, out_file, rois='all', out_index=None):
 
 
 if __name__ == '__main__':
+    atlas = Atlas('HCP-MMP')
     ROI_scalar(
         src_file=pjoin(proj_dir, 'data/HCP/HCPD_myelin.dscalar.nii'),
-        atlas=Atlas('HCP-MMP'), metric='mean',
-        out_file=pjoin(work_dir, 'HCPD-myelin_HCP-MMP_new.csv')
-    )
-    # ROI_scalar(
-    #     src_file=pjoin(proj_dir, 'data/HCP/HCPD_thickness.dscalar.nii'),
-    #     atlas=Atlas('HCP-MMP'), metric='mean',
-    #     out_file=pjoin(work_dir, 'HCPD-thickness_HCP-MMP.csv')
-    # )
-    # ROI_scalar(
-    #     src_file=pjoin(proj_dir, 'data/HCP/HCPA_myelin.dscalar.nii'),
-    #     atlas=Atlas('HCP-MMP'), metric='mean',
-    #     out_file=pjoin(work_dir, 'HCPA-myelin_HCP-MMP.csv')
-    # )
-    # ROI_scalar(
-    #     src_file=pjoin(proj_dir, 'data/HCP/HCPA_thickness.dscalar.nii'),
-    #     atlas=Atlas('HCP-MMP'), metric='mean',
-    #     out_file=pjoin(work_dir, 'HCPA-thickness_HCP-MMP.csv')
-    # )
-    ROI_scalar(
-        src_file=pjoin(anal_dir, 'gdist/gdist_src-OccipitalPole.dscalar.nii'),
-        atlas=Atlas('HCP-MMP'), metric='mean',
-        out_file=pjoin(work_dir, 'gdist_src-OccipitalPole_HCP-MMP.csv')
-    )
-    ROI_scalar(
-        src_file=pjoin(anal_dir, 'decomposition/HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj.dscalar.nii'),
-        atlas=Atlas('HCP-MMP'), metric='mean',
-        out_file=pjoin(work_dir, 'HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj_HCP-MMP.csv'),
-        rois=get_rois('MMP-vis3-R'), out_index='map_name'
+        mask=atlas.maps[0], values=list(atlas.roi2label.values()),
+        metric='mean', rois=list(atlas.roi2label.keys()),
+        out_file=pjoin(work_dir, 'HCPD-myelin_HCP-MMP.csv')
     )
