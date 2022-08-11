02c5f7f65e5b1bf80a3d2d6130934cdeaf9e0756
sunshineDrizzle <954830460@qq.com>
2022-08-10 22:50:04 -0400

    cxy_visual_dev: update

--- cxy_visual_dev/scripts/calc_mask_map.py
+++ cxy_visual_dev/scripts/calc_mask_map.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 import nibabel as nib
 from os.path import join as pjoin
 from matplotlib import pyplot as plt
@@ -46,12 +47,11 @@ def mask_maps(data_file, mask, out_file):
                reader2.map_names(), label_tables=reader2.label_tables())
 
 
-def make_mask1():
+def make_mask1(N):
     """
     将HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj的PC1和PC2分段
     以值排序，然后切割成N段顶点数量基本相同的片段
     """
-    N = 2
     src_file = pjoin(anal_dir, 'decomposition/HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj.dscalar.nii')
     map_names = ['C1', 'C2']
     mask = Atlas('HCP-MMP').get_mask(get_rois('MMP-vis3-R'))[0]
@@ -182,9 +182,177 @@ def make_mask3():
                volume=reader.volume, label_tables=[lbl_tab])
 
 
-if __name__ == '__main__':
+def make_mask4():
+    """
+    依据各ROI在HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj中的PC1的值排序分成6级
+    """
+    n_level = 6
+    fpath = pjoin(anal_dir, 'ROI_scalar/ROI_scalar1_MMP-vis3-R.csv')
     atlas = Atlas('HCP-MMP')
-    mask = atlas.get_mask(get_rois('MMP-vis3-L') + get_rois('MMP-vis3-R'))[0]
+    out_file = pjoin(work_dir, 'HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj_6level-ROI.dlabel.nii')
+
+    df = pd.read_csv(fpath, index_col=0)
+    idx = 'mean_stru-C1'
+    rois_list = [[] for _ in range(n_level)]
+    rois_list[0].append('R_V1')
+    rois_list[1].append('R_V2')
+
+    lbl_tab = nib.cifti2.Cifti2LabelTable()
+    lbl_tab[0] = nib.cifti2.Cifti2Label(0, '???', 1, 1, 1, 0)
+    cmap = plt.cm.jet
+    color_indices = np.linspace(0, 1, n_level)
+    out_map = np.zeros((1, LR_count_32k), np.uint8)
+    for roi in df.columns:
+        x = df.loc[idx, roi]
+        if df.loc[idx, 'R_V2'] < x <= df.loc[idx, 'R_V3']:
+            rois_list[2].append(roi)
+        elif df.loc[idx, 'R_V3'] < x <= df.loc[idx, 'R_MT']:
+            rois_list[3].append(roi)
+        elif df.loc[idx, 'R_MT'] < x < df.loc[idx, 'R_PIT']:
+            rois_list[4].append(roi)
+        elif df.loc[idx, 'R_PIT'] <= x < df.loc[idx, 'R_TF']:
+            rois_list[5].append(roi)
+
+    for key, rois in enumerate(rois_list, 1):
+        out_map[atlas.get_mask(rois)] = key
+        lbl = nib.cifti2.Cifti2Label(key, f'level{key}',
+                                     *cmap(color_indices[key-1]))
+        lbl_tab[key] = lbl
+
+    reader = CiftiReader(mmp_map_file)
+    save2cifti(out_file, out_map, reader.brain_models(), label_tables=[lbl_tab])
+
+
+def make_mask5():
+    """
+    依据各ROI在HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj中的PC1的值排序分成5级
+    """
+    n_level = 5
+    fpath = pjoin(anal_dir, 'ROI_scalar/ROI_scalar1_MMP-vis3-R.csv')
+    atlas = Atlas('HCP-MMP')
+    out_file = pjoin(work_dir, 'HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj_5level-ROI.dlabel.nii')
+
+    df = pd.read_csv(fpath, index_col=0)
+    idx = 'mean_stru-C1'
+    rois_list = [[] for _ in range(n_level)]
+    rois_list[0].append('R_V1')
+
+    lbl_tab = nib.cifti2.Cifti2LabelTable()
+    lbl_tab[0] = nib.cifti2.Cifti2Label(0, '???', 1, 1, 1, 0)
+    cmap = plt.cm.jet
+    color_indices = np.linspace(0, 1, n_level)
+    out_map = np.zeros((1, LR_count_32k), np.uint8)
+    for roi in df.columns:
+        x = df.loc[idx, roi]
+        if df.loc[idx, 'R_V2'] <= x <= df.loc[idx, 'R_V3']:
+            rois_list[1].append(roi)
+        elif df.loc[idx, 'R_V3'] < x <= df.loc[idx, 'R_MT']:
+            rois_list[2].append(roi)
+        elif df.loc[idx, 'R_MT'] < x < df.loc[idx, 'R_PIT']:
+            rois_list[3].append(roi)
+        elif df.loc[idx, 'R_PIT'] <= x < df.loc[idx, 'R_TF']:
+            rois_list[4].append(roi)
+
+    for key, rois in enumerate(rois_list, 1):
+        out_map[atlas.get_mask(rois)] = key
+        lbl = nib.cifti2.Cifti2Label(key, f'level{key}',
+                                     *cmap(color_indices[key-1]))
+        lbl_tab[key] = lbl
+
+    reader = CiftiReader(mmp_map_file)
+    save2cifti(out_file, out_map, reader.brain_models(), label_tables=[lbl_tab])
+
+
+def make_mask6():
+    """
+    依据各ROI在HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj中的PC1的值排序分成4级
+    """
+    n_level = 4
+    fpath = pjoin(anal_dir, 'ROI_scalar/ROI_scalar1_MMP-vis3-R.csv')
+    atlas = Atlas('HCP-MMP')
+    out_file = pjoin(work_dir, 'HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj_4level-ROI.dlabel.nii')
+
+    df = pd.read_csv(fpath, index_col=0)
+    idx = 'mean_stru-C1'
+    rois_list = [[] for _ in range(n_level)]
+    rois_list[0].append('R_V1')
+
+    lbl_tab = nib.cifti2.Cifti2LabelTable()
+    lbl_tab[0] = nib.cifti2.Cifti2Label(0, '???', 1, 1, 1, 0)
+    cmap = plt.cm.jet
+    color_indices = np.linspace(0, 1, n_level)
+    out_map = np.zeros((1, LR_count_32k), np.uint8)
+    for roi in df.columns:
+        x = df.loc[idx, roi]
+        if df.loc[idx, 'R_V2'] <= x <= df.loc[idx, 'R_MT']:
+            rois_list[1].append(roi)
+        elif df.loc[idx, 'R_MT'] < x < df.loc[idx, 'R_PIT']:
+            rois_list[2].append(roi)
+        elif df.loc[idx, 'R_PIT'] <= x < df.loc[idx, 'R_TF']:
+            rois_list[3].append(roi)
+
+    for key, rois in enumerate(rois_list, 1):
+        out_map[atlas.get_mask(rois)] = key
+        lbl = nib.cifti2.Cifti2Label(key, f'level{key}',
+                                     *cmap(color_indices[key-1]))
+        lbl_tab[key] = lbl
+
+    reader = CiftiReader(mmp_map_file)
+    save2cifti(out_file, out_map, reader.brain_models(), label_tables=[lbl_tab])
+
+
+def make_mask7(N):
+    """
+    不包含TF和PeEc
+    将HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj的PC1和PC2分段
+    以值排序，然后切割成N段顶点数量基本相同的片段
+    """
+    src_file = pjoin(anal_dir, 'decomposition/HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj.dscalar.nii')
+    map_names = ['C1', 'C2']
+    rois = get_rois('MMP-vis3-R')
+    rois.remove('R_TF')
+    rois.remove('R_PeEc')
+    mask = Atlas('HCP-MMP').get_mask(rois)[0]
+    out_file = pjoin(work_dir, f'HCPY-M+T_MMP-vis3-R_zscore1_PCA-subj_mask7-N{N}.dlabel.nii')
+
+    n_vtx = np.sum(mask)
+    step = int(np.ceil(n_vtx / N))
+    bounds = np.arange(0, n_vtx, step)
+    bounds = np.r_[bounds, n_vtx]
+    print(bounds)
+
+    n_map = len(map_names)
+    reader = CiftiReader(src_file)
+    src_maps = reader.get_data()[:n_map]
+    assert map_names == reader.map_names()[:n_map]
+
+    lbl_tabs = []
+    cmap = plt.cm.jet
+    color_indices = np.linspace(0, 1, N)
+    out_maps = np.zeros_like(src_maps, np.uint8)
+    for map_idx in range(n_map):
+        data = src_maps[map_idx, mask]
+        vtx_indices = np.argsort(data)
+        lbl_tab = nib.cifti2.Cifti2LabelTable()
+        lbl_tab[0] = nib.cifti2.Cifti2Label(0, '???', 1, 1, 1, 0)
+        for s_idx, s_bound in enumerate(bounds[:-1]):
+            e_idx = s_idx + 1
+            e_bound = bounds[e_idx]
+            batch = vtx_indices[s_bound:e_bound]
+            data[batch] = e_idx
+            lbl = nib.cifti2.Cifti2Label(e_idx, f'{s_bound}~{e_bound}',
+                                         *cmap(color_indices[s_idx]))
+            lbl_tab[e_idx] = lbl
+        out_maps[map_idx, mask] = data
+        lbl_tabs.append(lbl_tab)
+
+    save2cifti(out_file, out_maps, reader.brain_models(), map_names,
+               reader.volume, lbl_tabs)
+
+
+if __name__ == '__main__':
+    # atlas = Atlas('HCP-MMP')
+    # mask = atlas.get_mask(get_rois('MMP-vis3-L') + get_rois('MMP-vis3-R'))[0]
     # mask_maps(
     #     data_file=s1200_avg_RFsize,
     #     mask=mask,
@@ -210,15 +378,21 @@ if __name__ == '__main__':
     #     mask=mask,
     #     out_file=pjoin(work_dir, 'gdist_src-OpMt_MMP-vis3.dscalar.nii')
     # )
-    mask_maps(
-        data_file=pjoin(anal_dir, 'variation/MMP-vis3_ring1-CS1_R_width5.dlabel.nii'),
-        mask=mask,
-        out_file=pjoin(work_dir, 'MMP-vis3_ring1-CS1_R_width5_mask-MMP-vis3.dlabel.nii')
-    )
+    # mask_maps(
+    #     data_file=pjoin(anal_dir, 'variation/MMP-vis3_ring1-CS1_R_width5.dlabel.nii'),
+    #     mask=mask,
+    #     out_file=pjoin(work_dir, 'MMP-vis3_ring1-CS1_R_width5_mask-MMP-vis3.dlabel.nii')
+    # )
 
-    # make_mask1()
+    # make_mask1(N=2)
+    # make_mask1(N=3)
+    make_mask1(N=5)
     # make_mask2()
     # make_mask3()
+    # make_mask4()
+    # make_mask5()
+    # make_mask6()
+    make_mask7(N=4)
 
     # atlas = Atlas('HCP-MMP')
     # R2_mask = nib.load(s1200_avg_R2).get_fdata()[0, :LR_count_32k] > 9.8
